c880855450cfda802800ba6b7faef0bc3b3e592b
이주성 <32183520@dankook.ac.kr>
2023-02-11 13:34:27 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    public static void init() throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n-543);
+
+    }
+
+    private static void pro() {
+
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro();
+
+        br.close();
+        bw.close();
+    }
+}
