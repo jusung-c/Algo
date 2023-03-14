c2bb35dc9076591d60d66b06624fc612a6d74387
이주성 <32183520@dankook.ac.kr>
2023-03-14 11:34:25 +0900

    [Silver I] Title: 단지번호붙이기, Time: 144 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,110 +1,128 @@
-/*
-1. 아이디어
-- 2중 for문으로 1을 찾아나가는 BFS
-- BFS 실행할 때마다 단지수 ++, 각 단지 안 집수++
-
-2. 시간복잡도
-V: 25*25
-E: 4*V
-O(V+E) = O(5V) = O(5^5) < 1초
-
-3. 자료구조
-방문 기록 : bool[][] visited
-지도 : int[][] map
-큐(BFS)
-
-*/
-
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, group_cnt;
+    static String[] a;
+    static boolean[][] visit;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static ArrayList<Integer> group;
 
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static int cnt, area;
-    static Queue<Pair> q = new LinkedList<Pair>();
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
+    static class Point {
+        int x;
+        int y;
 
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
-
-        map = new int[N+1][N+1];
-        for(int i=0; i<N; i++) {
-            String s = br.readLine();
-            for(int j=0; j<N; j++) {
-                map[i][j] = s.charAt(j) - '0';
-            }
-        }
-
-        visited = new boolean[N+1][N+1];
-
-        int[] areas = new int[N*N];
-        int z=0;
-
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                if(!visited[i][j] && map[i][j] == 1) {
-                    areas[z++] = BFS(i,j);
-                }
-            }
-        }
-
-        Arrays.sort(areas);
-
-        bw.write(cnt+"\n");
-        for(int i=0; i<N*N; i++) {
-            if (areas[i] != 0) {
-                bw.write(areas[i]+"\n");
-            }
-        }
-
-        bw.close();
-    }
-
-    private static int BFS(int i, int j) {
-        cnt++;
-        area =1;
-        visited[i][j] = true;
-        q.add(new Pair(i, j));
-
-        while (!q.isEmpty()) {
-            Pair curr = q.poll();
-
-            for(int k=0; k<4; k++) {
-                int n_x = curr.x + dx[k];
-                int n_y = curr.y + dy[k];
-
-                if (n_x < 0 || n_x > N || n_y < 0 || n_y > N) {
-                    continue;
-                }
-
-                if (!visited[n_x][n_y] && map[n_x][n_y] == 1) {
-                    q.add(new Pair(n_x, n_y));
-                    visited[n_x][n_y] = true;
-                    area++;
-                }
-
-            }
-        }
-
-        return area;
-
-    }
-
-    private static class Pair {
-        int x, y;
-        public Pair(int x, int y) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
+    }
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        a = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            a[i] = br.readLine();
+        }
+
+        visit = new boolean[N][N];
+    }
+
+    // x, y를 갈 수 있다는 걸 알고 방문한 상태
+    static void dfs(int x, int y) {
+        // 단지에 속한 집의 개수 증가, visit 체크
+        group_cnt++;
+        visit[x][y] = true;
+
+        // 인접한 집으로 새로운 방문
+        for (int k = 0; k < 4; k++) {
+            // 좌표 이동
+            int nx = x + dir[k][0];
+            int ny = y + dir[k][1];
+
+            // 새로 이동한 좌표가 지도에 존재하는 지 확인
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+            // 새로 이동한 좌표에 집이 있는 지 확인
+            if (a[nx].charAt(ny) == '0') continue;
+
+            // 새로 이동한 좌표의 집을 방문한 적이 있는 지 확인
+            if(visit[nx][ny]) continue;
+
+            dfs(nx, ny);
+        }
+
+    }
+
+    static void bfs(int x, int y) {
+        Queue<Point> que = new LinkedList<>();
+
+        group_cnt = 1;
+        que.add(new Point(x, y));
+        visit[x][y] = true;
+
+        while (!que.isEmpty()) {
+            Point p = que.poll();
+
+            for (int k = 0; k < 4; k++) {
+                // 좌표 이동
+                int nx = p.x + dir[k][0];
+                int ny = p.y + dir[k][1];
+
+                // 새로 이동한 좌표가 지도에 존재하는 지 확인
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+
+                // 새로 이동한 좌표에 집이 있는 지 확인
+                if (a[nx].charAt(ny) == '0') continue;
+
+                // 새로 이동한 좌표의 집을 방문한 적이 있는 지 확인
+                if(visit[nx][ny]) continue;
+
+                que.add(new Point(nx, ny));
+                visit[nx][ny] = true;
+                group_cnt++;
+            }
+        }
+
+
+
+    }
+
+    private static void pro() throws IOException {
+        group = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                // 방문하지 않은 집을 발견했을 때!
+                if (!visit[i][j] && a[i].charAt(j) == '1') {
+                    // 단지 안 집 개수
+                    group_cnt = 0;
+                    // 그 집을 기준으로 연결된 모든 집 탐색
+                    bfs(i, j);
+
+                    group.add(group_cnt);
+                }
+            }
+        }
+
+        Collections.sort(group);
+
+        bw.write(group.size() + "\n");
+
+        Iterator<Integer> iter = group.iterator();
+        while (iter.hasNext()) {
+            bw.write(iter.next() + "\n");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro();
+
+        bw.close();
     }
 }
