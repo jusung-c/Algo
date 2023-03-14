38c3c48c2c578283c0551ddad08240154b12577a
이주성 <32183520@dankook.ac.kr>
2023-03-14 12:37:00 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 564 ms, Memory: 117152 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int V, E, ans;
+    static int map[][];
+    static boolean visit[];
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine(), " ");
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        map = new int[V + 1][V + 1];
+        for (int i = 1; i <= E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            map[x][y] = 1;
+            map[y][x] = 1;
+        }
+
+        visit = new boolean[V + 1];
+    }
+
+    private static void pro() throws IOException {
+        for (int i = 1; i <= V; i++) {
+            if (visit[i]) continue;
+            dfs(i);
+            ans++;
+        }
+
+        bw.write(ans + " ");
+
+    }
+
+    private static void dfs(int x) {
+        visit[x] = true;
+
+        for (int y = 1; y <= V; y++) {
+            if (map[x][y] == 0) continue;
+            if (visit[y]) continue;
+
+            dfs(y);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro();
+
+        bw.close();
+    }
+}
