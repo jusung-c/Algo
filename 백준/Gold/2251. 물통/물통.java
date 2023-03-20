ffc9c3b37511a71f483927b192711478f2f05be4
이주성 <32183520@dankook.ac.kr>
2023-03-20 16:48:47 +0900

    [Gold V] Title: 물통, Time: 152 ms, Memory: 23652 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -3,42 +3,47 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-class State {
-    int[] X;
-
-    State(int[] XX) {
-        X = new int[3];
-        for (int i = 0; i < 3; i++) X[i] = XX[i];
-    }
-
-    State move(int from, int to, int[] Limit) {
-        State nX = new State(X);
-
-        // 물통이 꽉찬 경우
-        if (X[from] + X[to] >= Limit[to]) {
-            nX.X[to] = Limit[to];
-            nX.X[from] = nX.X[from] - (Limit[to] - X[to]);
-        } else {
-            nX.X[from] = 0;
-            nX.X[to] = X[from] + X[to];
-        }
-
-        return nX;
-    }
-}
-
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
     static int[] Limit;
-    static boolean[][][] visit;
     static boolean[] possible;
+    static boolean[][][] visit;
+
+    static class State {
+        int[] X = new int[3];
+
+        // 생성자
+        public State(int[] newX) {
+            for (int i = 0; i < 3; i++) {
+                X[i] = newX[i];
+            }
+        }
+
+        // 물 붓기
+        State move(int from, int to, int[] Limit) {
+            // from -> to 물 붓기
+            int[] nX = new int[]{X[0], X[1], X[2]};
+
+            // to 물통이 꽉차는 경우
+            if (X[from] + X[to] >= Limit[to]) {
+                nX[to] = Limit[to];
+                nX[from] = X[from] - (Limit[to] - X[to]);
+            } else {
+                nX[from] = 0;
+                nX[to] = X[from] + X[to];
+            }
+
+            return new State(nX);
+        }
+    }
+
 
     public static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine(), " ");
 
         Limit = new int[3];
-        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < 3; i++) {
             Limit[i] = Integer.parseInt(st.nextToken());
         }
@@ -54,33 +59,40 @@ public class Main {
             if (possible[i]) bw.write(i + " ");
         }
 
-
     }
 
-    private static void bfs(int A, int B, int C) {
-        Queue<State> Q = new LinkedList<>();
-        Q.add(new State(new int[]{A, B, C}));
-        visit[A][B][C] = true;
+    private static void bfs(int x1, int x2, int x3) {
+        Queue<State> q = new LinkedList<>();
 
-        while (!Q.isEmpty()) {
-            State s = Q.poll();
+        visit[x1][x2][x3] = true;
+        q.add(new State(new int[]{x1, x2, x3}));
+
+        while (!q.isEmpty()) {
+            State s = q.poll();
+
+            // A 물통이 비어있을 경우 C 상태 정답 기록
             if (s.X[0] == 0) possible[s.X[2]] = true;
 
+            // 가능한 6가지의 경우 모두 물 부어보기!
             for (int from = 0; from < 3; from++) {
                 for (int to = 0; to < 3; to++) {
-                    if(from == to) continue;
+                    // 같은 물통끼리는 부을 수 없으니까 제외
+                    if (from == to) continue;
 
+                    // 물 부은 결과
                     State ns = s.move(from, to, Limit);
 
-                    if (visit[ns.X[0]][ns.X[1]][ns.X[2]]) continue;
+                    // 이미 방문했던 상태면 제외!
+                    if (visit[ns.X[0]][ns.X[1]][ns.X[2]]) {
+                        continue;
+                    }
 
                     visit[ns.X[0]][ns.X[1]][ns.X[2]] = true;
-                    Q.add(ns);
+                    q.add(ns);
                 }
             }
-
-
         }
+
     }
 
     public static void main(String[] args) throws Exception {
