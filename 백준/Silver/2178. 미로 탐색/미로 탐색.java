811d973674ec7a64f8e1cabbb7b5c3fe6f8de701
이주성 <32183520@dankook.ac.kr>
2023-03-22 17:37:55 +0900

    [Silver I] Title: 미로 탐색, Time: 152 ms, Memory: 16552 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,100 +1,90 @@
-// 언어 : JAVA , (성공/실패) : 1/0
-// 메모리 : 18652 KB , 시간 : 396 ms
-
-/*
-// 1. 아이디어
-- (1,1)에서 BFS
-- BFS 돌때마다 cnt++, 최솟값 갱신
-
-// 2. 시간 복잡도
-O(V+E)
-V: 100*100
-E: 4*V
-O(V+E) = O(5V) = 5*100*100 < 1초
-
-// 3. 자료 구조
-- 방문 bool[][] visited
-- 큐 (BFS)
-- map : int[][]
-- Pair 클래스
-*/
-
-
 import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static int N, M, ans;
+    static int[][] map, dist;
+    static int[][] dir = new int[][]{
+            {1, 0}, {0, 1},
+            {-1, 0}, {0, -1}
+    };
 
-    static int N, M, MAX, cnt;
-    static int[][] map;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    static boolean[][] visited;
-    static Queue<Pair> q = new LinkedList<Pair>();
+    static class Point {
+        int x;
+        int y;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        MAX = Math.max(N, M);
-
-        map = new int[MAX+1][MAX+1];
-
-        for(int i=0; i<N; i++) {
-            String s = br.readLine();
-            for(int j=0; j<M; j++) {
-                map[i][j] = s.charAt(j) - '0';
-            }
-        }
-
-        BFS(0, 0);
-
-        bw.write(map[N-1][M-1]+" ");
-
-        bw.close();
-    }
-
-    static void BFS(int x, int y) {
-        visited = new boolean[MAX+1][MAX+1];
-        cnt = 0;
-
-        visited[x][y] = true;
-        q.offer(new Pair(x, y));
-
-        while (!q.isEmpty()) {
-            Pair curr = q.poll();
-
-            for(int k=0; k<4; k++) {
-                int n_x = curr.x + dx[k];
-                int n_y = curr.y + dy[k];
-
-                if (n_x < 0 || n_x >= N || n_y < 0 || n_y >= M) {
-                    continue;
-                }
-
-                if (!visited[n_x][n_y] && map[n_x][n_y] == 1) {
-                    q.offer(new Pair(n_x, n_y));
-                    map[n_x][n_y] = map[curr.x][curr.y] +1;
-                    visited[n_x][n_y] = true;
-                }
-            }
-        }
-
-    }
-
-
-    private static class Pair {
-        int x,y;
-        public Pair(int x, int y) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
+    }
+
+    public static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        dist = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+    }
+
+    private static void pro() throws IOException {
+        Point S = new Point(0, 0);
+        bfs(S);
+
+        bw.write(dist[N - 1][M - 1] + " ");
+    }
+
+    private static void bfs(Point start) {
+        Queue<Point> Q = new LinkedList<>();
+
+        // dist 배열은 갈 수 없으면 -1 이므로 일단 -1로 초기화 시켜둔다.
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                dist[i][j] = -1;
+            }
+        }
+
+        // 시작점을 Q에 넣고, visit 처리와 dist 값을 초기화한다.
+        Q.add(start);
+        dist[start.x][start.y] = 1;
+
+        while (!Q.isEmpty()) {
+            Point p = Q.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int nx = p.x + dir[k][0];
+                int ny = p.y + dir[k][1];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (dist[nx][ny] != -1) continue;
+                if (map[nx][ny] == 0 ) continue;
+
+                Q.add(new Point(nx, ny));
+                dist[nx][ny] = dist[p.x][p.y] + 1;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro();
+
+        bw.close();
     }
 }
