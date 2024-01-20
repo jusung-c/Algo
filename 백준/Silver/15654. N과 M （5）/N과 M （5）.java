402274691392d16f62900492832e2ad7187cb831
이주성 <32183520@dankook.ac.kr>
2024-01-20 14:03:15 +0900

    [Silver III] Title: N과 M (5), Time: 384 ms, Memory: 35436 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N, M;
+    static int[] selected;
+    static int[] nums;
+    static boolean[] used;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        nums = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        selected = new int[M + 1];
+        used = new boolean[N + 1];
+
+        rec_func(1);
+
+        br.close();
+        bw.close();
+    }
+
+    static void rec_func(int k) throws IOException {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                bw.write(selected[i]+" ");
+            }
+            bw.write("\n");
+
+        } else {
+            for (int cand = 1; cand <= N; cand++) {
+                if(used[cand]) continue;
+                selected[k] = nums[cand];
+                used[cand] = true;
+                rec_func(k + 1);
+                used[cand] = false;
+            }
+        }
+    }
+}
+
