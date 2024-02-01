602fc55e776df7d6c58ea35dc2ffe72ac6bca3d6
이주성 <32183520@dankook.ac.kr>
2024-02-01 11:40:20 +0900

    [Silver II] Title: N과 M (11), Time: 688 ms, Memory: 102628 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+/*
+    0. 문제
+        - N개의 자연수 중에서 M개를 고른 수열
+        - 같은 수를 여러 번 골라도 된다.
+
+    1. 아이디어
+        - 주어진 수 정렬
+        - k번째 자리에 i라는 수를 골랐으면 다음 탐색 범위 시작 위치를 i로 설정
+        - LinkedHashSet으로 중복 방지
+
+    2. 시간 복잡도
+        - N과 M의 최댓값이 8이므로 8! < 1초 가능한 접근법
+
+ */
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
+    static StringTokenizer st;
+
+    static int N, M;
+    static int[] num;
+    static LinkedHashSet<String> map = new LinkedHashSet<>();
+
+    public static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num);
+    }
+
+    private static void pro(int k, String numString) throws IOException {
+        if (k == M + 1) {
+            map.add(numString);
+        } else {
+            for (int i = 1; i <= N; i++) {
+                int n = num[i];
+
+                pro(k + 1, numString + n + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro(1, "");
+
+        for (String s : map) {
+            bw.write(s + "\n");
+        }
+
+        bw.close();
+    }
+}
