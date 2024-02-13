8902b3c71092eafda12074bb299013e446bfe15a
이주성 <32183520@dankook.ac.kr>
2024-02-13 11:51:23 +0900

    [Gold V] Title: 두 용액, Time: 376 ms, Memory: 28384 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,57 +1,66 @@
-// 언어 : JAVA , (성공/실패) : 1/0 
-// 메모리 : 18652 KB , 시간 : 396 ms
-
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+    static int[] list;
 
-    static int a, b;
-
-    public static void main(String[] args) throws IOException {
+    static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
-        
+        list = new int[N + 1];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        for(int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    static void pro() throws IOException {
+        // list 정렬
+        Arrays.sort(list, 1, N + 1);
+
+        // 투 포인터
+        int L = 1;
+        int R = N;
+        int r1 = 0;
+        int r2 = 0;
+        int ans = Integer.MAX_VALUE;
+
+        // L == R의 경우는 한 용액만 남는 경우임
+        while (L < R) {
+            int sum = list[L] + list[R];
+
+            // ans, r1, r2 갱신
+            if (Math.abs(sum) < ans) {
+                // 비교하기 쉽도록 절댓값으로 갱신
+                ans = Math.abs(sum);
+                r1 = list[L];
+                r2 = list[R];
+            }
+
+            // 최소와 최대를 더했을 때 0보다 큰 경우
+            if (sum > 0) {
+                R--;
+
+            // 최소와 최대를 더했을 때 0보다 작은 경우
+            } else {
+                L++;
+            }
         }
 
-        Arrays.sort(arr);
-        
-        Result(arr, N);
+        bw.write(r1+" "+r2);
+    }
 
-        bw.write(a+" "+b);
+    public static void main(String[] args) throws IOException {
+        init();
+
+        pro();
+
         bw.close();
     }
 
-    public static void Result(int[] arr, int n) {
-        int left = 0;
-        int right = n-1;
-        int max = 2000000000;
-
-        while (left < right) {
-            int sum = arr[left]+arr[right];
-            if (Math.abs(sum) < max) {
-                a = arr[left];
-                b = arr[right];
-                max = Math.abs(sum);
-            }
-
-            if(sum > 0) {
-                right--;
-            } else {
-                left++;
-            }
-
-
-        }
-
-    }
 }
