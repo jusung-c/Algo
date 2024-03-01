020a1644c359a3a15f30e33368fc034992e94b9e
이주성 <32183520@dankook.ac.kr>
2024-03-01 16:08:08 +0900

    [Gold V] Title: 입국심사, Time: 360 ms, Memory: 23404 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -2,14 +2,28 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/*
+    0. 문제
+        - 조건: M명이 N개의 입국심사대에서 심사
+        - 타겟: 걸리는 시간 T
+
+    1. 아이디어
+        - 주어진 시간 T 동안 M명이 N개의 입국심사대를 통과할 수 있는가?
+        -
+
+    2. 시간 복잡도
+
+ */
+
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
     static int N, M;
     static int[] time;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
 
         st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
@@ -19,21 +33,7 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             time[i] = Integer.parseInt(br.readLine());
         }
-    }
 
-    private static boolean YesOrNo(long T) {
-        long sum = 0;
-
-        for (int i = 1; i <= N; i++) {
-            sum += T / time[i];
-            
-            // 오버플로우 신경쓰기
-            if (sum >= M) {
-                return true;
-            }
-        }
-
-        return sum >= M;
     }
 
     private static void pro() throws IOException {
@@ -44,16 +44,31 @@ public class Main {
         while (start <= end) {
             long mid = (start + end) / 2;
 
-            if (YesOrNo(mid)) {
+            if (yesOrNo(mid)) {
                 result = mid;
+
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
 
-        bw.write(result + " ");
+        bw.write(result + "");
 
+    }
+
+    private static boolean yesOrNo(long T) {
+        long sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sum += T / time[i];
+
+            if (sum >= M) {
+                return true;
+            }
+        }
+
+        return sum >= M;
     }
 
     public static void main(String[] args) throws Exception {
