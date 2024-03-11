1af630a69ff2095dbc4ac8b3ca0fde09cc9409e5
이주성 <32183520@dankook.ac.kr>
2024-03-11 18:15:36 +0900

    [Silver V] Title: 배열 합치기, Time: 1488 ms, Memory: 328244 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,73 +1,86 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+/*
+    0. 문제
+
+    1. 아이디어
+
+    2. 시간 복잡도
+
+ */
+
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
+    static StringTokenizer st;
     static int N, M;
     static int[] A, B, result;
 
-    static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
         A = new int[N + 1];
+        B = new int[M + 1];
+        result = new int[N + M + 1];
+
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        st = new StringTokenizer(br.readLine());
-        B = new int[M + 1];
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= M; i++) {
             B[i] = Integer.parseInt(st.nextToken());
         }
 
-        result = new int[N + M + 1];
     }
 
-    static void pro() {
-        int L = 1;
-        int R = 1;
-        int i = 1;
+    private static void pro() throws IOException {
+        int A_idx = 1;
+        int B_idx = 1;
+        int result_idx = 1;
 
-        while (L <= N && R <= M) {
-            if (A[L] <= B[R]) {
-                result[i] = A[L];
-                i++;
-                L++;
+        while (A_idx <= N && B_idx <= M) {
+            if (A[A_idx] <= B[B_idx]) {
+                result[result_idx] = A[A_idx];
+                A_idx++;
+                result_idx++;
             } else {
-                result[i] = B[R];
-                i++;
-                R++;
+                result[result_idx] = B[B_idx];
+                B_idx++;
+                result_idx++;
             }
         }
 
-        while (L <= N) {
-            result[i] = A[L];
-            i++;
-            L++;
+        if (A_idx != N + 1) {
+            for (int i = A_idx; i <= N; i++) {
+                result[result_idx] = A[i];
+                result_idx++;
+            }
+        } else {
+            for (int i = B_idx; i <= M; i++) {
+                result[result_idx] = B[i];
+                result_idx++;
+            }
         }
 
-        while (R <= M) {
-            result[i] = B[R];
-            i++;
-            R++;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        init();
-
-        pro();
 
         for (int i = 1; i <= result.length - 1; i++) {
             bw.write(result[i] + " ");
         }
 
-        bw.close();
     }
 
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro();
+
+        bw.close();
+    }
 }
