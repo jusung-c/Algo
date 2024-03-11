b90882868761d4edfc49be18d7f0e456ce3c0082
이주성 <32183520@dankook.ac.kr>
2024-03-11 17:29:34 +0900

    [Silver I] Title: 귀여운 라이언, Time: 444 ms, Memory: 73264 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,57 +1,64 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+/*
+    0. 문제
+
+    1. 아이디어
+
+    2. 시간 복잡도
+
+ */
+
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
+    static StringTokenizer st;
     static int N, K;
-    static int[] nums;
+    static int[] dolls;
 
-    static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        nums = new int[N + 1];
+        dolls = new int[N + 1];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            dolls[i] = Integer.parseInt(st.nextToken());
         }
+
     }
 
-    static void pro() throws IOException {
+    private static void pro() throws IOException {
         int R = 0;
-        int ans = 0;
-        int result = Integer.MAX_VALUE;
+        int cnt = 0;
+        int size = N + 1;
 
         for (int L = 1; L <= N; L++) {
-
-            if (nums[L-1] == 1) {
-                ans--;
+            if (dolls[L - 1] == 1) {
+                cnt--;
             }
 
-            while (R + 1 <= N && ans < K) {
+            while (R + 1 <= N && cnt < K) {
                 R++;
-                if (nums[R] == 1) {
-                    ans++;
-                }
+                if(dolls[R] == 1) cnt++;
             }
 
-            if (ans >= K) {
-                result = Math.min(result, R - L + 1);
-            }
-
+            if (cnt >= K) size = Math.min(size, R - L + 1);
         }
 
-        if (result > 0 && result <= N) {
-            bw.write(result + " ");
+        if (size == N + 1) {
+            bw.write("-1");
         } else {
-            bw.write(-1 + " ");
+            bw.write(size + " ");
         }
+
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         init();
 
         pro();
