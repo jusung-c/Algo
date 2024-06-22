a33e8db968677029a918aea954deff93baaed820
이주성 <32183520@dankook.ac.kr>
2024-06-23 05:20:47 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.05 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -4,22 +4,47 @@ class Solution {
         
         int row = 0;
         int col = 0;
-        arr[row][col] = 1;
+        int val = 1;
+        int direction = 0;
         
-        for (int i=2; i<=n*n; i++) {    
-            if (col + 1 < n && arr[row][col+1] == 0 && (row == 0 || arr[row-1][col] != 0)) {
-                col++;
-                arr[row][col] = i;
-            } else if (row + 1 < n && arr[row+1][col] == 0) {
-                row++;
-                arr[row][col] = i;
-            } else if (col - 1 >= 0 && arr[row][col-1] == 0) {
-                col--;
-                arr[row][col] = i;
-            } else if (row - 1 >= 0 && arr[row-1][col] == 0) {
-                row--;
-                arr[row][col] = i;
+        while (val <= n*n) {
+            arr[row][col] = val++;
+            
+            if (direction == 0 ) {
+                if (col + 1 >= n || arr[row][col+1] != 0) {
+                    direction = 1;
+                    row++;
+                } else {
+                    col++;
+                }
+            } else if (direction == 1) {
+                if (row + 1 >= n || arr[row+1][col] != 0) {
+                    direction = 2;
+                    col--;
+                } else {
+                    row++;
+                }
+                
+            } else if (direction == 2) {
+                if (col <= 0 || arr[row][col-1] != 0) {
+                    direction = 3;
+                    row--;
+                } else {
+                    col--;
+                }
+                
+            } else {
+                if (row <= 0 || arr[row-1][col] !=0) {
+                    direction = 0;
+                    col++;
+                } else {
+                    row--;
+                }
+
+                
             }
+            
+            
             
         }
         
