25f8e2a5a4840ebe2c37fddc93febcb090ccf01d
이주성 <32183520@dankook.ac.kr>
2024-06-23 06:01:21 +0900

    [level 0] Title: l로 만들기, Time: 12.78 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,17 +1,12 @@
+import java.util.stream.*;
+
 class Solution {
     public String solution(String myString) {
-        String answer = "";
         
-        for (int i=0; i< myString.length(); i++) {
-            if ((int) myString.charAt(i) < (int)'l') {
-                answer += "l";
-            } else {
-                answer += myString.charAt(i);
+        return myString.chars()
+            .mapToObj(i -> Character.toString(Integer.max(i, 'l')))
+            .collect(Collectors.joining());
 
-            }
-            
-        }
         
-        return answer;
     }
 }
