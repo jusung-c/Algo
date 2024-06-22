c2b950a6c4ac601714cfbcfe03908a194513e50c
이주성 <32183520@dankook.ac.kr>
2024-06-23 04:23:19 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 2.75 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,13 +1,11 @@
+import java.util.stream.*;
+
 class Solution {
     public int solution(int[][] arr) {
-        int answer = 1;
         
-        for (int i=0; i<arr.length; i++) {
-            for (int j = i + 1; j<arr[0].length; j++) {
-                if (arr[i][j] != arr[j][i]) return 0;
-            }
-        }
+        return IntStream.range(0, arr.length)
+            .allMatch(i -> IntStream.range(i + 1, arr[0].length)
+                     .allMatch(j -> arr[i][j] == arr[j][i])) ? 1 : 0;  
         
-        return answer;
     }
 }
