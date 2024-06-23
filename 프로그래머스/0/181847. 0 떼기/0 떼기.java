8fb35fb9d55462a5db7e725f560417cc66601611
이주성 <32183520@dankook.ac.kr>
2024-06-23 20:42:49 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,21 +1,16 @@
 class Solution {
     public String solution(String n_str) {
-        String answer = "";
         int index = -1;
         
         for (int i=0; i<n_str.length(); i++) {
             char c = n_str.charAt(i);
             
-            if (c != '0') break;
-            else index = i;
+            if (c != '0') {
+                index = i;
+                break;
+            }
         }
 
-        if (index == -1) return n_str;
-        
-        for(int i = index + 1; i<n_str.length(); i++) {
-            answer += n_str.charAt(i);
-        }
-        
-        return answer;
+        return n_str.substring(index);
     }
 }
