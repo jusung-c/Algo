7a30bd92d4904ecc9af17fc5b97a0c8ab51462a0
이주성 <32183520@dankook.ac.kr>
2024-06-24 16:29:51 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 11.58 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int answer[] = new int[k];
+        Arrays.fill(answer , -1);
+        
+        int set[] = Arrays.stream(arr).distinct().toArray();
+        
+        if (set.length == k) return set;
+        else if (set.length > k) {
+            for(int i=0; i<k; i++) {
+                answer[i] = set[i];
+            }
+        } else {
+            for(int i=0; i<set.length; i++) {
+                answer[i] = set[i];
+            }
+        }
+        
+        return answer;
+    }
+}
