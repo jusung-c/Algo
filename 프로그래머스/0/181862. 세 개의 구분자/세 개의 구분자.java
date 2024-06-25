cdb1c4106f29f580fa90e45df0ae4794ed2979eb
이주성 <32183520@dankook.ac.kr>
2024-06-25 14:48:35 +0900

    [level 0] Title: 세 개의 구분자, Time: 158.42 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -10,8 +10,6 @@ class Solution {
             .filter(s -> !s.isEmpty())
             .toArray(String[]::new);
         
-        if (answer.length == 0) return new String[]{"EMPTY"};
-        
-        return answer;
+        return answer.length == 0 ? new String[]{"EMPTY"} : answer;
     }
 }
