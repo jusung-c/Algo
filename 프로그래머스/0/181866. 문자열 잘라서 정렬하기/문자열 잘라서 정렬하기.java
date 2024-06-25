82e3acbd10ab4336d6f9bf01111b05a85b140e09
이주성 <32183520@dankook.ac.kr>
2024-06-25 15:40:16 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 22.21 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myString) {
+        String answer[] = myString.split("[x]+");
+        answer = Arrays.stream(answer)
+            .filter(s -> !s.isEmpty())
+            .toArray(String[]::new);
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
