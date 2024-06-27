158d1cfa8d62065f7a00dfa3d9d274ace49c2481
이주성 <32183520@dankook.ac.kr>
2024-06-27 09:43:15 +0900

    [level 0] Title: 배열 만들기 4, Time: 8.87 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        
+        for (int i=0; i<arr.length; i++) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                continue;
+            }
+            
+            if (stk.get(stk.size()-1) < arr[i]) {
+                stk.add(arr[i]);
+            } else {
+                stk.remove(stk.size()-1);
+                i--;
+            }
+        }
+        
+        return stk.stream().mapToInt(i ->i).toArray();
+    }
+}
