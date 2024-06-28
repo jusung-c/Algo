cb0e1f08af02b7872c23ca573a08f846d0423703
이주성 <32183520@dankook.ac.kr>
2024-06-28 10:26:21 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 11.90 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -3,27 +3,18 @@ import java.util.stream.*;
 
 class Solution {
     public String solution(String[] my_strings, int[][] parts) {
-        String answer = "";
-        
-        IntStream.range(0, my_strings.length)
-            .mapToObj(i -> {
-                String str = my_strings[i];
-                int[] arr = parts[i];
+//         return IntStream.range(0, my_strings.length)
+//             .mapToObj(i -> {
+//                 String str = my_strings[i];
+//                 int[] arr = parts[i];
 
-                return IntStream.range(arr[0], arr[1]+1)
-                    .mapToObj(index -> String.valueOf(str.charAt(index)))
-                    .collect(Collectors.joining());
-            }).collect(Collectors.joining());
+//                 return IntStream.range(arr[0], arr[1]+1)
+//                     .mapToObj(index -> String.valueOf(str.charAt(index)))
+//                     .collect(Collectors.joining());
+//             }).collect(Collectors.joining());
         
-        for(int i=0; i<my_strings.length; i++) {
-            String str = my_strings[i];
-            int[] arr = parts[i];
-            
-            answer += IntStream.range(arr[0], arr[1]+1)
-                .mapToObj(index -> String.valueOf(str.charAt(index)))
-                .collect(Collectors.joining());
-        }
-        
-        return answer;
+        return IntStream.range(0, parts.length)
+            .mapToObj(i -> my_strings[i].substring(parts[i][0], parts[i][1] + 1))
+            .collect(Collectors.joining());
     }
 }
