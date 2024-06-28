898d7970a79cdd4b8fd335055da48b911405eab1
이주성 <32183520@dankook.ac.kr>
2024-06-29 01:45:32 +0900

    [level 0] Title: 리스트 자르기, Time: 2.41 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,26 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        switch(n) {
+            case 1:
+                return IntStream.rangeClosed(0, slicer[1]).map(i -> num_list[i]).toArray();
+                
+            case 2:
+                return IntStream.range(slicer[0], num_list.length).map(i -> num_list[i]).toArray();
+                
+            case 3:
+                return IntStream.rangeClosed(slicer[0], slicer[1]).map(i -> num_list[i]).toArray();
+                
+            case 4:
+                if (slicer[2] == 1) 
+                    return IntStream.rangeClosed(slicer[0], slicer[1]).map(i -> num_list[i]).toArray();
+                return IntStream.rangeClosed(slicer[0], slicer[1])
+                    .filter(i -> (i - slicer[0]) % slicer[2] == 0)
+                    .map(i -> num_list[i])
+                    .toArray();
+            default:
+                return null;
+        }    
+    }
+}
