74548b6a396510e8b5fd6b265032c57ba0d5b3d6
이주성 <32183520@dankook.ac.kr>
2024-06-28 10:41:34 +0900

    [level 0] Title: 접미사 배열, Time: 2.05 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String[] solution(String my_string) {        
+        String[] answer = IntStream.range(0, my_string.length())
+            .mapToObj(i -> my_string.substring(i))
+            .toArray(String[]::new);
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
