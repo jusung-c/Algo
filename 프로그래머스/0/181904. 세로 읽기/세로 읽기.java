85ddd6aca91fa8ba302cda3dbd324205fa5d199f
이주성 <32183520@dankook.ac.kr>
2024-06-28 11:46:47 +0900

    [level 0] Title: 세로 읽기, Time: 3.20 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -3,9 +3,8 @@ import java.util.stream.*;
 class Solution {
     public String solution(String my_string, int m, int c) {
         return IntStream.range(0, my_string.length())
-            .filter(i -> i % m == 0)
-            .filter(i -> i+c-1 < my_string.length())
-            .mapToObj(i -> String.valueOf(my_string.charAt(i+c-1)))
+            .filter(i -> i % m == c-1)
+            .mapToObj(i -> String.valueOf(my_string.charAt(i)))
             .collect(Collectors.joining());
     }
 }
