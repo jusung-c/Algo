a47b9eefcdbce32c8a13cda7ef83a1f377eeadbc
이주성 <32183520@dankook.ac.kr>
2024-06-30 17:42:03 +0900

    [level 0] Title: ad 제거하기, Time: 5.08 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,10 @@
+import java.util.stream.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        return IntStream.range(0, strArr.length)
+            .filter(i -> !strArr[i].contains("ad"))
+            .mapToObj(i -> strArr[i])
+            .toArray(String[]::new);
+    }
+}
