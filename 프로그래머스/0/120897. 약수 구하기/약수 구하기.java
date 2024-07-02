b70a640e115ded6d68b4f9e2797fdd56d5d89be6
이주성 <32183520@dankook.ac.kr>
2024-07-02 10:13:00 +0900

    [level 0] Title: 약수 구하기, Time: 2.22 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,13 +1,9 @@
-import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        
-        for(int i=1; i<=n; i++) {
-            if (n % i == 0) answer.add(i);
-        }
-        
-        return answer.stream().mapToInt(i -> i).toArray();
+        return IntStream.rangeClosed(1, n)
+            .filter(i -> n % i == 0)
+            .toArray();
     }
 }
