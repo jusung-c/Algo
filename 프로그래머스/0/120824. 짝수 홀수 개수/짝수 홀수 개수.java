73a6fc6faddb1231f56a5ac8123a00cc20fe4d06
이주성 <32183520@dankook.ac.kr>
2024-07-02 09:54:35 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = {0, 0}; 
-        for(int i=0; i<num_list.length; i++) {
-            if(num_list[i] % 2 ==0) {
-                answer[0]++;
-            }
-        }
+        int[] answer = new int[2];
         
-        answer[1] = num_list.length - answer[0];
+        for (int n : num_list) {
+            if (n % 2 == 0) 
+                answer[0]++;
+            else
+                answer[1]++;
+        }
         
         return answer;
     }
