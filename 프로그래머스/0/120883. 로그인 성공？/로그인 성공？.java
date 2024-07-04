5b48a7ae56506863c4943945ae7cba138533c5bb
이주성 <32183520@dankook.ac.kr>
2024-07-04 11:18:25 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String id = id_pw[0];
+        String pw = id_pw[1];
+        String answer = "fail";
+        
+        for(String[] user : db) {
+            if (user[0].equals(id)) {
+                if (user[1].equals(pw)) {
+                    return "login";
+                } else {
+                    return "wrong pw";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
