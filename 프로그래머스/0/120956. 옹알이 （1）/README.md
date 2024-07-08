87b709736b3caab2ed4b97b5d00ebe79d10b8697
이주성 <32183520@dankook.ac.kr>
2024-07-08 10:02:06 +0900

    [level 0] Title: 옹알이 (1), Time: 1.72 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/README.md
+++ 프로그래머스/0/120956. 옹알이 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옹알이 (1) - 120956 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956#) 
 
 ### 성능 요약
 
-메모리: 79.9 MB, 시간: 2.37 ms
+메모리: 77.8 MB, 시간: 1.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 08일 09:24:05
+2024년 07월 08일 10:02:02
 
 ### 문제 설명
 
--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,10 +3,13 @@ class Solution {
         int answer = 0;
         
         for (String s : babbling) {
-            s = s.replaceAll("aya", " ");
-            s = s.replaceAll("ye", " ");
-            s = s.replaceAll("woo", " ");
-            s = s.replaceAll("ma", " ");
+            s = s.replaceFirst("aya", " ");
+            s = s.replaceFirst("ye", " ");
+            s = s.replaceFirst("woo", " ");
+            s = s.replaceFirst("ma", " ");
+            
+            System.out.println(s);
+            
             s = s.replaceAll("[ ]+", "");
             
             System.out.println(s);
