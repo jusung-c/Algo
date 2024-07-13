868ea8b44d6cd64a07dec4ebbd20e036b968e774
이주성 <32183520@dankook.ac.kr>
2024-07-13 21:44:11 +0900

    [level 1] Title: 시저 암호, Time: 2.06 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,26 +1,30 @@
 class Solution {
     public String solution(String s, int n) {
-
-        StringBuilder builder = new StringBuilder();
         
+        // 연산이 끝난 문자들을 이어붙이기 위한 StringBuilder
+        StringBuilder sb = new StringBuilder();
+        
+        // 문자들을 하나씩 암호화한다.
         for (char c : s.toCharArray()) {
-            // c를 n만큼 밀어서 builder에 붙이기
-            builder.append(push(c, n));
+            // 암호화한 문자를 이어 붙이기
+            sb.append(encryt(c, n));
         }
         
-        return builder.toString();
+        return sb.toString();
     }
     
-    public char push(char c, int n) {
-        if (c == ' ') return c;
+    public char encryt(char c, int n) {
+        // 공백의 경우는 그대로 공백
+        if (!Character.isAlphabetic(c)) return c;
         
+        // 0을 기준으로 맞춰주기 (대소문자 구별)
         int offset = Character.isUpperCase(c) ? 'A' : 'a';
-        int position = c - offset;  // 0~25의 수로 표현 
+        int position = c - offset;
         
-        // 26개의 알파벳이므로 나머지 연산으로 위치 찾기
-        position = (position + n) % ('z' - 'a' + 1);
+        // n만큼 더해준 후 나머지 연산으로 인덱스 구하기
+        position = (position + n) % 26;
         
-        // offset과 찾은 위치를 더해 최종 위치 찾기
-        return (char) (offset + position);
+        // 인덱스에 offset 더해줘서 실제 문자 반환
+        return (char) (position + offset);
     }
 }
