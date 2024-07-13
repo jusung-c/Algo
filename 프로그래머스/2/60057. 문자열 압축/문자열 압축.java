72e38147898356f6fe00c5a80e1f975ff01b62df
이주성 <32183520@dankook.ac.kr>
2024-07-14 00:30:57 +0900

    [level 2] Title: 문자열 압축, Time: 8.29 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -4,17 +4,18 @@ class Solution {
     public int solution(String s) {
         int answer = Integer.MAX_VALUE;
         
-        // 압축 단위
-        for (int unit=1; unit<=s.length(); unit++) {
-            // unit 단위로 토큰 배열 생성
+        // 1~문자열의 길이까지 압축 단위 설정해 반복
+        for(int unit=1; unit<=s.length(); unit++) {
+            
+            // 문자열을 압축 단위로 쪼개서 토큰 배열 생성
             List<String> tokens = split(s, unit);
             
-            // token 배열을 반복해서 문자열 압축
-            String compressed = compress(tokens, unit);
+            // 토큰 배열을 돌면서 문자열 압축
+            String compressed = compress(tokens);
             
-            // 압축한 문자열 길이 최소값 갱신
-            answer = answer > compressed.length() ? compressed.length() : answer;
-                
+            // 압축한 문자열의 길이 최소값 갱신 
+            answer = answer > compressed.length() ?
+                compressed.length() : answer;
         }
         
         return answer;
@@ -23,44 +24,42 @@ class Solution {
     public List<String> split(String s, int unit) {
         List<String> tokens = new ArrayList<>();
         
-        for(int i=0; i<s.length(); i += unit) {
-            // 남은 원소 개수가 unit보다 작은 경우는 남은것만 추가
-            if (i + unit > s.length()) 
+        for (int i=0; i<s.length(); i+=unit) {
+            // 남은 원소의 개수가 unit보다 작은 경우
+            if (i + unit >= s.length())
                 tokens.add(s.substring(i));
             else
-                // unit 수만큼 잘라서 추가
-                tokens.add(s.substring(i, i + unit));          
+                // unit만큼 잘라서 토큰으로 추가
+                tokens.add(s.substring(i, i+unit));
         }
         
         return tokens;
     }
     
-    public String compress(List<String> tokens, int unit) {
+    public String compress(List<String> tokens) {
         StringBuilder sb = new StringBuilder();
         
         String last = "";
-        int count = 0;
+        int cnt = 0;
         
-        // token을 돌면서 개수 세기
         for (String token : tokens) {
-            // 이전 토큰과 같을 경우
-            if (last.equals(token)) {
-                count++;
-                
-            // 이전 토큰과 다를 경우
+            // 카운팅
+            if (token.equals(last)) {
+                cnt++;
             } else {
-                if (count > 1) sb.append(count);
-                
+                // 이전 토큰 정보 압축
+                if (cnt > 1) sb.append(cnt);
                 sb.append(last);
                 
+                // 새로운 토큰 기준으로 다시 카운팅 시작
                 last = token;
-                count = 1;
+                cnt = 1;
             }
         }
         
-        // 마지막 남은 1개 토큰 처리
-        if (count > 1) sb.append(count);
-        sb.append(last);
+        // 남은 마지막 토큰 처리
+        if (cnt > 1) sb.append(cnt);
+            sb.append(last);
         
         return sb.toString();
     }
