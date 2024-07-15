0483804f22ab64cd97dcc85f507362b336f2f8b3
이주성 <32183520@dankook.ac.kr>
2024-07-15 11:03:07 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.21 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean solution(String s) {
-        if (s.length() != 4 && s.length() != 6) return false;
-        
-        String ns = s.replaceAll("[^0-9]", "");
-        
-        return s.length() == ns.length();
+        return s.matches("[0-9]{4}|[0-9]{6}");
     }
 }
