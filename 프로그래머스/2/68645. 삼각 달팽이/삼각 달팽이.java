3a8dbe4079af790eb309f4696a89f017a6c5d0d8
이주성 <32183520@dankook.ac.kr>
2024-07-26 15:10:27 +0900

    [level 2] Title: 삼각 달팽이, Time: 56.15 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,54 +1,58 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     private static final int[] dx = {0, 1, -1};
     private static final int[] dy = {1, 0, -1};
-    
+
     public int[] solution(int n) {
+
         ArrayList<Integer> answer = new ArrayList<>();
+
         int[][] map = new int[n][n];
         int v = 1;
         int y = 0;
         int x = 0;
         int d = 0;
-        
+
         // 1. 방향 순서대로 진행하면서 칸 채우기
         while (true) {
             map[y][x] = v++;
-            
+
+            // 방향을 바꿀 것인지 진행한 방향 그대로 갈 것인지 판단해야 한다.
+
+            // 일단 방향 그대로 진행할 경우 범위 및 이미 방문한 곳인지 확인한다.
             int nx = x + dx[d];
             int ny = y + dy[d];
-            
-            // 범위 및 방문 체크
-            if (nx == n || nx == -1 || ny == n | ny == -1 ||
-               map[ny][nx] != 0) {
-                
-                d = (d+1) % 3;
+            if (nx == n || nx == -1 || ny == n || ny == -1
+                    || map[ny][nx] != 0) {
+
+                // 방향 전환해야할 경우 전환할 방향도 마찬가지로 검증한다.
+                d = (d + 1) % 3;
                 nx = x + dx[d];
                 ny = y + dy[d];
-                
-                // 범위 및 방문 체크
-                if (nx == n || nx == -1 || ny == n | ny == -1 ||
-                    map[ny][nx] != 0) break;
-                
-                x = nx;
-                y = ny;
+                if (nx == n || nx == -1 || ny == n || ny == -1
+                        || map[ny][nx] != 0) {
+
+                    // 방향을 전환해도 이동할 수 없다면 무한 루프를 탈출한다.
+                    break;
+                }
             }
             
             x = nx;
             y = ny;
             
         }
-        
-        // 2. 하나의 정수 배열로 변환하기
-        for(int[] list : map) {
-            for(int num : list) {
-                if (num != 0) answer.add(num);
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer.add(map[i][j]);
             }
         }
-        
-        return answer.stream().mapToInt(i -> i).toArray();
-        
+
+        return answer.stream()      // Stream<Integer>
+                .mapToInt(Integer::intValue)    // IntStream
+                .toArray();         // int[]
     }
 }
-
