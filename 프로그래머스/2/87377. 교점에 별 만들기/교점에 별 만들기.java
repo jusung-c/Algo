4948ba1ce73e4c7ffc6594d48e6d3e30cb4e4106
이주성 <32183520@dankook.ac.kr>
2024-07-26 14:24:54 +0900

    [level 2] Title: 교점에 별 만들기, Time: 140.43 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,88 +1,96 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    private static class Point {
-        public final long x, y;
-        
-        private Point(long x, long y) {
+    long maxX = Long.MIN_VALUE;
+    long maxY = Long.MIN_VALUE;
+    long minX = Long.MAX_VALUE;
+    long minY = Long.MAX_VALUE;
+
+    private class Point {
+        private final long x, y;
+
+        public Point(long x, long y) {
             this.x = x;
             this.y = y;
         }
     }
     
     public String[] solution(int[][] line) {
-        int n = line.length;
-        
-        ArrayList<Point> points = new ArrayList<>();
-        
-        long maxX = Long.MIN_VALUE;
-        long maxY = Long.MIN_VALUE;
-        long minX = Long.MAX_VALUE;
-        long minY = Long.MAX_VALUE;
-        
-        // 1. 주어진 직선들의 모든 교점 찾기
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                
-                // 정수인 교점 구하기
-                Point p = func(line[i][0], line[i][1], line[i][2]
-                    ,line[j][0] ,line[j][1] ,line[j][2]);
-                
-                if (p != null) points.add(p);
+
+        List<Point> pointList = new ArrayList<>();
+
+        // 1. 주어진 좌표의 교점 구하기
+        for (int i = 0; i < line.length; i++) {
+            for (int j = i + 1; j < line.length; j++) {
+                Point p = getPoint(line[i][0], line[i][1], line[i][2],
+                        line[j][0], line[j][1], line[j][2]);
+
+                if (p != null) pointList.add(p);
             }
         }
-        
-        // 2. 교점들의 최대x, 최소x, 최대y, 최소y 값 찾기
-        for (Point p : points) {
+
+        // 2. 사각형 생성
+        String[][] angle = makeAngle(pointList);
+
+        // 3. 교점을 사각형에 찍기
+        // 사분면의 교점을 배열로 옮기기 위해
+        // x축은 (p.x - minX)로 음수 값들을 다 오른쪽으로 넘기고
+        // y축은 (p.y - maxY)로 양수 값들을 다 아래쪽으로 넘긴다.
+        // 이 때 배열에서의 y 좌표는 사분면과 부호가 반대이므로 -(p.y - maxY)로 처리한다.
+        for (Point p : pointList) {
+            int newX = (int) (p.x - minX);
+            int newY = (int) (maxY - p.y);
+
+            angle[newY][newX] = "*";
+        }
+
+        // 4. 문자열 배열로 변환
+        String[] answer = new String[angle.length];
+        for (int i = 0; i < angle.length; i++) {
+            answer[i] = Arrays.stream(angle[i])                  // Stream<String>
+                    .collect(Collectors.joining(""));   // String
+
+        }
+
+        return answer;
+    }
+
+    private String[][] makeAngle(List<Point> pointList) {
+        for (Point p : pointList) {
             maxX = Math.max(maxX, p.x);
-            minX = Math.min(minX, p.x);
             maxY = Math.max(maxY, p.y);
+            minX = Math.min(minX, p.x);
             minY = Math.min(minY, p.y);
         }
-        
-        // 최소 사각형의 가로, 세로 길이
+
         int width = (int) (maxX - minX + 1);
         int height = (int) (maxY - minY + 1);
-        
-        // 사각형 배열 초기에 "." 으로 초기화
+
         String[][] angle = new String[height][width];
-        
+
+        // "."으로 초기화
         for (String[] list : angle) {
             Arrays.fill(list, ".");
         }
-        
-        // 교점을 돌면서 별찍기
-        for (Point p : points) {
-            
-            // 사분면 위 좌표 기준인 p를 
-            // (0, 0) ~ (-width, height) 범위의 사각형으로 변환
-            int x = (int) (p.x - minX);
-            int y = (int) (maxY - p.y);
-            angle[y][x] = "*";
-        }
-        
-        String[] answer = new String[angle.length];
-        for (int i=0; i<angle.length; i++) {
-            answer[i] = "";
-            for (int j=0; j<angle[i].length; j++) {
-                answer[i] += angle[i][j];
-            }
-        }
-        
-        return answer;
+
+        return angle;
     }
-    
-    public Point func(double A, double B, double E
-                      , double C, double D, double F) {
-        
-        if (A*D - B*C == 0) return null;
-        
-        double x = (B*F-E*D) / (A*D-B*C);
-        double y = (E*C-A*F) / (A*D-B*C);
-        
-        // 정수인 좌표만 사용
+
+    private Point getPoint(double A, double B, double E,
+                           double C, double D, double F) {
+
+        // 1-1. 평행 or 일치 확인
+        if (A * D - B * C == 0) return null;
+
+
+        // 1-2. 교점 구하기
+        double x = (B * F - E * D) / (A * D - B * C);
+        double y = (E * C - A * F) / (A * D - B * C);
+
+        // 1-3. 정수인 경우만 사용
         if (x % 1 != 0 || y % 1 != 0) return null;
-        
+
         return new Point((long) x, (long) y);
     }
 }
