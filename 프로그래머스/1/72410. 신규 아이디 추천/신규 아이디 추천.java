0fa1ce3d9bf4b8cd1b05a803bcca398c6520bcbf
이주성 <32183520@dankook.ac.kr>
2024-07-30 17:12:10 +0900

    [level 1] Title: 신규 아이디 추천, Time: 25.46 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,24 +1,29 @@
 class Solution {
     public String solution(String new_id) {
+
+        // step 1
         new_id = new_id.toLowerCase();
-        
-        new_id = new_id.replaceAll("[^a-z0-9\\-\\_\\.]", "");
-        
+
+        // step 2
+        new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
+
+        // step 3
         new_id = new_id.replaceAll("[.]+", ".");
-        
+
+        // step 4
         new_id = new_id.replaceAll("^[.]|[.]$", "");
-        
-        new_id = new_id.isEmpty() ? "a":new_id;
-        
+
+        // step 5
+        new_id = new_id.isEmpty() ? "a" : new_id;
+
+        // step 6
         new_id = new_id.length() >= 16 ? new_id.substring(0, 15).replaceAll("[.]$", "") : new_id;
-        
-        
-        if (new_id.length() <= 2) {
-            for(int i=new_id.length(); i<3; i++) {
-                new_id += new_id.charAt(new_id.length()-1);
-            }
+
+        // step 7
+        while (new_id.length() <= 2) {
+            new_id += new_id.charAt(new_id.length() - 1);
         }
-        
+
         return new_id;
     }
 }
