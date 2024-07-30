4c50159cdf11313437753023f63c1f02d9b6dca8
이주성 <32183520@dankook.ac.kr>
2024-07-30 16:45:59 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 34.11 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[] solution(String s) {
-        int del = 0;
-        int cnt = 0;
-        
+        int zeroCnt = 0;
+        int changeCnt = 0;
+
+        // s가 "1"이 될때까지 반복한다.
         while (!s.equals("1")) {
-            // 0 제거
-            String nonZero = s.replace("0", "");
-            
-            // 제거한 0 개수 업데이트
-            del += s.length() - nonZero.length();
-            
-            // nonZero의 길이를 이진 변환
-            s = Integer.toString(nonZero.length(), 2);
-            cnt++;
+            // 1. x에서 0 제거
+            String after = s.replaceAll("0", "");
+            zeroCnt += s.length() - after.length();
+
+            System.out.println("after = " + after);
+
+            // 2. x의 길이를 이진변환
+            s = Integer.toString(after.length(), 2);
+            changeCnt++;
+
+            System.out.println("s = " + s);
         }
-        
-        return new int[]{cnt, del};
+
+        return new int[]{changeCnt, zeroCnt};
     }
 }
