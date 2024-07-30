4def7476e50bb06e4951cec4708ddeebe97c7b6a
이주성 <32183520@dankook.ac.kr>
2024-07-30 16:12:27 +0900

    [level 2] Title: 문자열 압축, Time: 8.38 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,47 +1,48 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int solution(String s) {
-        int answer = Integer.MAX_VALUE;
-        
-        // 1~문자열의 길이까지 압축 단위 설정해 반복
-        for(int unit=1; unit<=s.length(); unit++) {
-            
+        int min = Integer.MAX_VALUE;
+
+        // 문자열의 길이가 n이라면 n/2 길이로 압축하는 것이 최대
+        // 1~n/2 반복하면서 압축하고 제일 최소의 길이를 구하기
+        for (int n = 1; n <= s.length(); n++) {
+
             // 문자열을 압축 단위로 쪼개서 토큰 배열 생성
-            List<String> tokens = split(s, unit);
-            
-            // 토큰 배열을 돌면서 문자열 압축
+            List<String> tokens = split(s, n);
+
+            // 토큰을 돌면서 문자열 압축
             String compressed = compress(tokens);
-            
-            // 압축한 문자열의 길이 최소값 갱신 
-            answer = answer > compressed.length() ?
-                compressed.length() : answer;
+
+            min = Math.min(min, compressed.length());
         }
-        
-        return answer;
+
+        return min;
     }
-    
-    public List<String> split(String s, int unit) {
+
+    private List<String> split(String s, int n) {
         List<String> tokens = new ArrayList<>();
-        
-        for (int i=0; i<s.length(); i+=unit) {
-            // 남은 원소의 개수가 unit보다 작은 경우
-            if (i + unit >= s.length())
+
+        for (int i = 0; i < s.length(); i += n) {
+            // 남은 원소의 개수가 n보다 작은 경우
+            if (i + n >= s.length()) {
                 tokens.add(s.substring(i));
-            else
-                // unit만큼 잘라서 토큰으로 추가
-                tokens.add(s.substring(i, i+unit));
+            } else {
+                // n 길이만큼 잘라서 토큰으로 추가
+                tokens.add(s.substring(i, i + n));
+            }
         }
-        
+
         return tokens;
     }
-    
-    public String compress(List<String> tokens) {
+
+    private String compress(List<String> tokens) {
         StringBuilder sb = new StringBuilder();
-        
+
         String last = "";
         int cnt = 0;
-        
+
         for (String token : tokens) {
             // 카운팅
             if (token.equals(last)) {
@@ -50,17 +51,17 @@ class Solution {
                 // 이전 토큰 정보 압축
                 if (cnt > 1) sb.append(cnt);
                 sb.append(last);
-                
+
                 // 새로운 토큰 기준으로 다시 카운팅 시작
                 last = token;
                 cnt = 1;
             }
         }
-        
+
         // 남은 마지막 토큰 처리
         if (cnt > 1) sb.append(cnt);
-            sb.append(last);
-        
+        sb.append(last);
+
         return sb.toString();
     }
 }
