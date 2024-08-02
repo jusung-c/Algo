eeb387ebc51d2d3f9f59179b90e3ab1f15f0dbb2
이주성 <32183520@dankook.ac.kr>
2024-08-02 14:40:55 +0900

    [level 2] Title: 모음 사전, Time: 8.05 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,29 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    private static final char[] CHARS = "AEIOU".toCharArray();
-    
-    private List<String> generator(String word) {
-        List<String> words = new ArrayList<>();
+    // 1. 상태 정의
+    // 단어를 하나씩 이어 붙여서 만들어가야 하므로 현재까지 만든 단어를 word라고 할 때 (word)로 상태를 정의한다.
+    private void generator(String word, List<String> words) {
         words.add(word);
+
+        // 2. 종료 조건 정의
+        // 5글자의 단어가 완성되면 더 이상 붙일 수 없으므로 종료한다.
+        if (word.length() == 5) return;
         
-        // 종료 조건
-        if (word.length() == 5) return words;
-        
-        // 점화식
-        for (char c : CHARS) {
-            words.addAll(generator(word + c));
-        }
-        
-        return words;
+        // 3. 점화식
+        // A, E, I, O, U 순으로 단어를 붙여나간다.
+        generator(word + "A", words);
+        generator(word + "E", words);
+        generator(word + "I", words);
+        generator(word + "O", words);
+        generator(word + "U", words);
     }
-    
+
     public int solution(String word) {
-        return generator("").indexOf(word);
+        List<String> words = new ArrayList<>();
+        generator("", words);
+
+        return words.indexOf(word);
     }
 }
