821b7a58dcd77fcaaf128a9a453d1c3a6d0ca535
이주성 <32183520@dankook.ac.kr>
2024-08-02 14:27:08 +0900

    [level 2] Title: 하노이의 탑, Time: 1.95 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,25 +1,32 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-    public int[][] solution(int n) {
-        return hanoi(n, 1, 3).toArray(new int[0][]);
-        
+
+    // 1. 상태 정의
+    // 원판 n개를 from에서 to로 이동
+    private void hanoi(int n, int from, int to, List<int[]> answer) {
+
+        // 2. 종료 조건 탈출
+        // 원판은 하나씩만 옮길 수 있으므로 원판을 옮기는 과정을 담기 위해 n이 1인 경우 답 리스트에 추가
+        if (n == 1) {
+            answer.add(new int[]{from, to});
+            return;
+        }
+
+        // 3. 점화식 정의
+        int empty = 6 - (from + to);
+
+        hanoi(n - 1, from, empty, answer);
+        hanoi(1, from, to, answer);
+        hanoi(n - 1, empty, to, answer);
     }
-    
-    // 상태를 매개변수로 받는 재귀 메서드
-    private List<int[]> hanoi(int n, int from, int to) {
-        // 종료 조건
-        if (n==1) return List.of(new int[] {from, to});
-        
-        // 빈 기둥 번호 조회
-        int empty = 6 - from - to;
-        
-        List<int[]> result = new ArrayList<>();
-        
-        result.addAll(hanoi(n-1, from, empty));    // 1단계
-        result.addAll(hanoi(1, from, to));         // 2단계
-        result.addAll(hanoi(n-1, empty, to));      // 3단계
-        
-        return result;
+
+    public int[][] solution(int n) {
+        List<int[]> answer = new ArrayList<>();
+
+        hanoi(n, 1, 3, answer);
+
+        return answer.toArray(new int[0][]);
     }
 }
