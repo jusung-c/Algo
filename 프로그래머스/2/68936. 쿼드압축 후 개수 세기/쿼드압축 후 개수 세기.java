a9f7d83e6d4fb55220809175d3294440bde76102
이주성 <32183520@dankook.ac.kr>
2024-08-02 14:04:37 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 42.24 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,43 +1,45 @@
 class Solution {
-    class Count {
-        private int zeroCnt;
-        private int oneCnt;
+    // 1. 상태 정의
+    // (offsetX, offsetY)로 시작하고, 한 변의 길이가 size인 정사각형의 0과 1의 개수
+    private class Count {
+        int zero, one;
 
-        public Count(int zeroCnt, int oneCnt) {
-            this.zeroCnt = zeroCnt;
-            this.oneCnt = oneCnt;
+        public Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
         }
 
-        public Count add(Count other) {
-            return new Count(this.zeroCnt + other.zeroCnt, this.oneCnt + other.oneCnt);
+        public Count add(Count o) {
+            return new Count(zero + o.zero, one + o.one);
         }
     }
 
-    // 재귀 함수
-    private Count count(int offsetX, int offsetY, int size, int[][] arr) {
-        int h = size / 2;
+    private Count getCount(int offsetX, int offsetY, int size, int[][] arr) {
 
-        // 정사각형의 원소를 다 돌면서 체크
+        // 2. 종료 조건 탈출
+        // 정사각형 내의 원소가 모두 1이거나 모두 0일 때 종료
         for (int x = offsetX; x < offsetX + size; x++) {
             for (int y = offsetY; y < offsetY + size; y++) {
 
-                // 정사각형의 모든 원소가 같은 값이 아닐 경우
                 if (arr[y][x] != arr[offsetY][offsetX]) {
-                    return count(offsetX, offsetY, h, arr)
-                            .add(count(offsetX + h, offsetY, h, arr))
-                            .add(count(offsetX, offsetY + h, h, arr))
-                            .add(count(offsetX + h, offsetY + h, h, arr));
+                    int h = size / 2;
+
+                    // 3. 점화식 정의
+                    // 4개의 정사각형으로 쪼갠 뒤 재귀 호출로 압축된 결과를 더해주기
+                    return getCount(offsetX, offsetY, h, arr)
+                            .add(getCount(offsetX + h, offsetY, h, arr))
+                            .add(getCount(offsetX, offsetY + h, h, arr))
+                            .add(getCount(offsetX + h, offsetY + h, h, arr));
                 }
             }
         }
 
-        // 정시각형의 모든 원소가 다 같은 경우
+        // 종료 조건 탈출
         return arr[offsetY][offsetX] == 1 ? new Count(0, 1) : new Count(1, 0);
     }
 
     public int[] solution(int[][] arr) {
-        Count count = count(0, 0, arr.length, arr);
-
-        return new int[]{count.zeroCnt, count.oneCnt};
+        Count count = getCount(0, 0, arr.length, arr);
+        return new int[]{count.zero, count.one};
     }
 }
