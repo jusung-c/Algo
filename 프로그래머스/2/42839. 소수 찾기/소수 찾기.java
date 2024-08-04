cb4b3328f5d4a78910999858d9911cc8f89a316e
이주성 <32183520@dankook.ac.kr>
2024-08-04 16:27:52 +0900

    [level 2] Title: 소수 찾기, Time: 5.42 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,41 +1,53 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
+    public int solution(String numbers) {
+        int[] nums = numbers.chars()
+                .map(c -> c - '0')
+                .toArray();
+
+        boolean[] isUsed = new boolean[numbers.length()];
+
+        Set<Integer> primes = new HashSet<>();
+
+        // 1. 주어진 종이 조각의 수로 만들 수 있는 모든 조합 구하기
+
+        // 재귀로 모든 조합을 구해야 한다.
+        // 상태: 지금까지 만든 숫자를 acc, 남은 숫자들을 numbers라고 할 때 (acc, numbers)
+        // 종료 조건: 모든 숫자를 다 사용해서 더 이상 남은 숫자가 없는 경우. (acc, 0)
+        // 재귀: (acc*10+n, numbers-n)
+
+        getPrimes(0, nums, isUsed, primes);
+
+        return primes.size();
+    }
+
+    // 재귀 호출
+    private void getPrimes(int acc, int[] numbers, boolean[] isUsed, Set<Integer> primes) {
+        // 소수인지 판단
+        if (isPrime(acc)) primes.add(acc);
+
+        // 숫자 이어 붙이기
+        for (int i = 0; i < numbers.length; i++) {
+            if (isUsed[i]) continue;
+
+            int nextAcc = acc * 10 + numbers[i];
+            isUsed[i] = true;
+
+            getPrimes(nextAcc, numbers, isUsed, primes);
+
+            isUsed[i] = false;
+        }
+    }
+
     private boolean isPrime(int n) {
         if (n <= 1) return false;
-        
-        for (int i=2; i*i<=n; i++) {
+
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) return false;
         }
-        
+
         return true;
-    }
-    
-    private void getPrimes(int acc, List<Integer> numbers, Set<Integer> primes) {
-        if (isPrime(acc)) primes.add(acc); // 소수 판별
-        
-        // 숫자 이어붙이기
-        for (int i=0; i<numbers.size(); i++) {
-            int nextAcc = acc * 10 + numbers.get(i);
-            
-            List<Integer> nextNumbers = new ArrayList<>(numbers);
-            nextNumbers.remove(i);
-            
-            getPrimes(nextAcc, nextNumbers, primes);
-        }
-    }
-    
-    public int solution(String nums) {
-        
-        List<Integer> numbers = nums.chars()
-            .map(c -> c - '0')
-            .boxed()
-            .collect(Collectors.toList());
-        
-        Set<Integer> primes = new HashSet<>();
-        getPrimes(0, numbers, primes);
-        
-        return primes.size();
     }
 }
