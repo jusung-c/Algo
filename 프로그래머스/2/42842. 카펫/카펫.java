2622d8aa3e6c745b2ed22193977dfa4431ab8786
이주성 <32183520@dankook.ac.kr>
2024-08-04 15:18:33 +0900

    [level 2] Title: 카펫, Time: 10.22 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,22 @@
 class Solution {
-    
     public int[] solution(int brown, int yellow) {
-        
-        for (int x=3; x<=5000; x++) {
-            for (int y=3; y<=x; y++) {
-                // 조건 검사
-                int b_cnt = 2*x + 2*y - 4;
-                int y_cnt = (x-2)*(y-2);
-                
-                if (b_cnt == brown && y_cnt == yellow) {
-                    return new int[] {x, y};
+        int[] answer = {};
+
+        // 가로 길이 x, 세로 길이 y일 때
+        // brown: 2x + 2y - 4
+        // yellow: (x-2) * (y-2)
+
+        // 갈색과 노랑의 최소가 8, 1 이라는 것은 x와 y가 최소 3이라는 것. 즉, 3 <= y <= x
+        // 갈색의 최대가 5_000 이므로 가로의 최대가 2500을 넘지 않는다.
+        for (int x = 0; x < 2500; x++) {
+            for (int y = 0; y <= x; y++) {
+                if (brown == (2 * x + 2 * y - 4) && yellow == (x - 2) * (y - 2)) {
+                    return new int[]{x, y};
                 }
+
             }
         }
-        
-        return null;
+
+        return answer;
     }
 }
