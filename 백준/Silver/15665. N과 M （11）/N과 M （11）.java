6b817e20c27975ae5240b7d32fb4b72008712738
이주성 <32183520@dankook.ac.kr>
2024-08-05 18:32:05 +0900

    [Silver II] Title: N과 M (11), Time: 608 ms, Memory: 103436 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -1,59 +1,42 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.StringTokenizer;
-/*
-    0. 문제
-        - N개의 자연수 중에서 M개를 고른 수열
-        - 같은 수를 여러 번 골라도 된다.
-
-    1. 아이디어
-        - 주어진 수 정렬
-        - k번째 자리에 i라는 수를 골랐으면 다음 탐색 범위 시작 위치를 i로 설정
-        - LinkedHashSet으로 중복 방지
-
-    2. 시간 복잡도
-        - N과 M의 최댓값이 8이므로 8! < 1초 가능한 접근법
-
- */
+import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader br;
     static StringTokenizer st;
-
     static int N, M;
-    static int[] num;
-    static LinkedHashSet<String> map = new LinkedHashSet<>();
+    static int[] numbers;
+    static LinkedHashSet<String> answer = new LinkedHashSet<>();
 
     public static void init() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        num = new int[N + 1];
-
-        st = new StringTokenizer(br.readLine(), " ");
+        numbers = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(num);
+        Arrays.sort(numbers);
     }
 
-    private static void pro(int k, String numString) throws IOException {
+    private static void pro(int k, String result) throws IOException {
+        // 종료 조건
         if (k == M + 1) {
-            map.add(numString);
-        } else {
-            for (int i = 1; i <= N; i++) {
-                int n = num[i];
-
-                pro(k + 1, numString + n + " ");
-            }
+            answer.add(result);
+            return;
         }
+
+        for (int i = 1; i <= N; i++) {
+
+            pro(k + 1, result + numbers[i] + " ");
+        }
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -61,7 +44,7 @@ public class Main {
 
         pro(1, "");
 
-        for (String s : map) {
+        for (String s : answer) {
             bw.write(s + "\n");
         }
 
