54ece7819f2ae12edb86d420e59d1c7d867e5bff
이주성 <32183520@dankook.ac.kr>
2024-08-06 15:33:27 +0900

    [Silver I] Title: 스타트와 링크, Time: 288 ms, Memory: 31940 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,93 +1,95 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	
-	static int N;
-	static int[][] map;
-	static boolean[] visit;
-	
-	static int Min = Integer.MAX_VALUE;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		N = Integer.parseInt(br.readLine());
- 
-		map = new int[N][N];
-		visit = new boolean[N];
- 
- 
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
- 
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
- 
-		combi(0, 0);
-		System.out.println(Min);
- 
-	}
- 
-	// idx는 인덱스, count는 조합 개수(=재귀 깊이)
-	static void combi(int idx, int count) {
-		// 팀 조합이 완성될 경우
-		if(count == N / 2) {
-			/*
-			 방문한 팀과 방문하지 않은 팀을 각각 나누어
-			 각 팀의 점수를 구한 뒤 최솟값을 찾는다.
-			*/
-			diff();
-			return;
-		}
- 
-		for(int i = idx; i < N; i++) {
-			// 방문하지 않았다면?
-			if(!visit[i]) {
-				visit[i] = true;	// 방문으로 변경
-				combi(i + 1, count + 1);	// 재귀 호출
-				visit[i] = false;	// 재귀가 끝나면 비방문으로 변경
-			}
-		}
-	}
- 
-	// 두 팀의 능력치 차이를 계산하는 함수 
-	static void diff() {
-		int team_start = 0;
-		int team_link = 0;
- 
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = i + 1; j < N; j++) {
-				// i 번째 사람과 j 번째 사람이 true라면 스타트팀으로 점수 플러스 
-				if (visit[i] == true && visit[j] == true) {
-					team_start += map[i][j];
-					team_start += map[j][i];
-				}
-				// i 번째 사람과 j 번째 사람이 false라면 링크팀으로 점수 플러스 
-				else if (visit[i] == false && visit[j] == false) {
-					team_link += map[i][j];
-					team_link += map[j][i];
-				}
-			}
-		}
-		// 두 팀의 점수 차이 (절댓값)
-		int val = Math.abs(team_start - team_link);
-		
-		/*
-		  두 팀의 점수차가 0이라면 가장 낮은 최솟값이기 때문에
-		  더이상의 탐색 필요없이 0을 출력하고 종료하면 된다.
-		 */
-		if (val == 0) {
-			System.out.println(val);
-			System.exit(0);
-		}
-		
-		Min = Math.min(val, Min);
-				
-	}
- 
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N, M;
+    static int min = Integer.MAX_VALUE;
+    static int[] start, link;
+    static boolean[] isUsed;
+    static int[][] stat;
+
+    public static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        M = N / 2;
+
+        start = new int[M];
+        isUsed = new boolean[N];
+        stat = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                stat[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    private static void pro(int k, int prev) throws IOException {
+        // 종료 조건
+        if (k == M) {
+            int sub = calSub(start, isUsed);
+            min = Math.min(min, sub);
+            return;
+        }
+
+        for (int i = prev; i < N; i++) {
+            if (isUsed[i]) continue;
+
+            isUsed[i] = true;
+            start[k] = i;
+
+            pro(k + 1, i + 1);
+
+            isUsed[i] = false;
+            start[k] = 0;
+        }
+    }
+
+    private static int calSub(int[] start, boolean[] isUsed) throws IOException {
+        // link 팀 구하기
+        link = new int[M];
+
+        int index = 0;
+        for (int i = 0; i < N; i++) {
+            if (isUsed[i]) continue;
+            link[index++] = i;
+        }
+
+        // 각 팀의 점수 차이 구하기
+        int sub = Math.abs(getScore(start) - getScore(link));
+//        System.out.println("sub = " + sub);
+        return sub;
+    }
+
+    private static int getScore(int[] team) {
+        int result = 0;
+
+        for (int i = 0; i < M; i++) {
+            for (int j = i; j < M; j++) {
+                int x = team[i];
+                int y = team[j];
+
+                result += stat[x][y];
+                result += stat[y][x];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro(0, 0);
+
+        bw.write(min+" ");
+
+        bw.close();
+    }
 }
