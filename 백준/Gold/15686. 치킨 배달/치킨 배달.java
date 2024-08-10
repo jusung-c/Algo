e3b3e0cf13e63d54a0bef4d26af4578e04ea42d6
이주성 <32183520@dankook.ac.kr>
2024-08-10 20:20:30 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 18460 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,106 +1,86 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
-
-/*
-    0. 문제
-        - NxN 크기의 도시의 각 칸은 빈칸(0), 치킨집(2), 집(1) 중 하나이다.
-        - 집과 가장 가까운 치킨집까지의 거리를 치킨 거리라고 할 때
-        도시의 치킨 거리는 모든 집의 치킨 거리의 합이다.
-        - 도시에서 가장 수익을 많이 낼 수 있는 치킨집의 개수는 최대 M개일 때
-        M개를 제외한 치킨집은 폐업시킬 예정이다.
-        - 어떻게 M개의 치킨집을 골라야 도시의 치킨 거리가 최소가 될지 구하라
-
-    1. 아이디어
-        - 치킨집들 중 중복없이 순서없이 M개의 치킨집을 고른다.
-        - 치킨 거리를 계산한 후 최소값을 구한다.
-
-    2. 시간 복잡도
-        -
-
- */
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader br;
     static StringTokenizer st;
-    static int N, M;
-    static int answer = Integer.MAX_VALUE;
+    static int N, M, houseCnt, chickenCnt;
+    static int min = Integer.MAX_VALUE;
     static int[] selected;
-    static ArrayList<Pair> house = new ArrayList<>();
-    static ArrayList<Pair> chicken = new ArrayList<>();
-
-    static class Pair {
-        int x, y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static List<int[]> houses = new ArrayList<>();
+    static List<int[]> chickens = new ArrayList<>();
 
     public static void init() throws IOException {
-
         br = new BufferedReader(new InputStreamReader(System.in));
 
         st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        selected = new int[M + 1];
+        selected = new int[M];
 
-        for (int i = 1; i <= N; i++) {
+        for (int r = 1; r <= N; r++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= N; j++) {
-                int n = Integer.parseInt(st.nextToken());
-
-                if (n == 1) house.add(new Pair(i, j));
-                if (n == 2) chicken.add(new Pair(i, j));
+            for (int c = 1; c <= N; c++) {
+                switch (Integer.parseInt(st.nextToken())) {
+                    case 0:
+                        break;
+                    case 1:
+                        houses.add(new int[]{r, c});
+                        break;
+                    case 2:
+                        chickens.add(new int[]{r, c});
+                        break;
+                }
             }
         }
 
-
+        houseCnt = houses.size();
+        chickenCnt = chickens.size();
     }
 
     private static void pro(int k, int prev) throws IOException {
-        if (k == M + 1) {
-            answer = Math.min(answer, calculate());
-        } else {
-            for (int i = prev; i < chicken.size(); i++) {
-                selected[k] = i;
+        if (k == M) {
+            min = Math.min(min, getDist());
+            return;
+        }
 
-                pro(k + 1, i + 1);
+        for (int i = prev; i < chickenCnt; i++) {
+            selected[k] = i;
 
-                selected[k] = 0;
-            }
+            pro(k + 1, i + 1);
+
+            selected[k] = 0;
         }
 
     }
 
-    // 치킨 거리 계산
-    private static int calculate() {
-        int sum = 0;
+    private static int getDist() throws IOException {
+        int result = 0;
 
-        for (Pair p1 : house) {
-            int min = Integer.MAX_VALUE;
-            for (int j = 1; j <= M; j++) {
-                Pair p2 = chicken.get(selected[j]);
+        for (int[] h : houses) {
+            int minDist = Integer.MAX_VALUE;
 
-                min = Math.min(min, Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y));
+            for (int i = 0; i < M; i++) {
+                int[] c = chickens.get(selected[i]);
 
+                minDist = Math.min(minDist, Math.abs(c[0] - h[0]) + Math.abs(c[1] - h[1]));
             }
-            sum += min;
+
+            result += minDist;
         }
 
-        return sum;
+        return result;
     }
 
     public static void main(String[] args) throws Exception {
         init();
 
-        pro(1, 0);
-
-        bw.write(answer + "");
+        pro(0, 0);
+        bw.write(min + " ");
 
         bw.close();
     }
