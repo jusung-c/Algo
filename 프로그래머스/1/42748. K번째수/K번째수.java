69422b4487b164508b0b63925d8fa069a6ba07f1
이주성 <32183520@dankook.ac.kr>
2024-08-11 20:17:50 +0900

    [level 1] Title: K번째수, Time: 6.66 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,22 +1,24 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        
-        for (int index=0; index<commands.length; index++) {
-            int i = commands[index][0];
-            int j = commands[index][1];
-            int k = commands[index][2];
-            
-            int[] arr = Arrays.copyOfRange(array, i-1, j);
-            Arrays.sort(arr);
-            
-            answer[index] = arr[k-1];
-            
+
+        for (int i = 0; i < commands.length; i++) {
+
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int index = commands[i][2];
+
+            int[] sorted = IntStream.range(start - 1, end) // IntStream
+                    .map(n -> array[n])     // IntStream
+                    .sorted()               // IntStream
+                    .toArray();// int[]
+
+
+            answer[i] = sorted[index - 1];
         }
-        
+
         return answer;
     }
 }
