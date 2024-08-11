1399053cc7fa32ba277807d76b8b38147ccc9f9f
이주성 <32183520@dankook.ac.kr>
2024-08-11 21:41:49 +0900

    [level 2] Title: H-Index, Time: 0.90 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,20 +1,22 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.Arrays;
 
 class Solution {
-    
     public int solution(int[] citations) {
-        // 역순 정렬
-        Integer[] arr = Arrays.stream(citations)
-            .boxed().toArray(Integer[]::new);
-        Arrays.sort(arr, Collections.reverseOrder());
-        citations = Arrays.stream(arr)
-            .mapToInt(Integer::intValue).toArray();
-        
-        for (int i=0; i<citations.length; i++) {
-            if (citations[i] < i+1) return i;
+        // n개 중 h번 이상 인용된 논문이 h개 이상
+        // 나머지 논문이 h번 이하 인용되었을 때 h의 최댓값을 구하라
+
+        Arrays.sort(citations);
+        // 0, 1, 3, 5, 6
+
+        for (int h = citations.length; h >= 1; h--) {
+            if (isHIndex(citations, h)) return h;
         }
-        
-        return citations.length;
+
+        return 0;
+    }
+
+    private boolean isHIndex(int[] citations, int h) {
+        // 뒤에서부터 h번째는
+        return citations[citations.length - h] >= h;
     }
 }
