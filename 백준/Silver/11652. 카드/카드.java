aa2b215b702938a996b27c9ef48318d8f5828713
이주성 <32183520@dankook.ac.kr>
2024-08-14 12:44:23 +0900

    [Silver IV] Title: 카드, Time: 288 ms, Memory: 28896 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,53 +1,47 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
-    static int N, currentCnt, maxCnt;
-    static Long max = Long.MIN_VALUE;
-    static long[] nums;
-    public static void init() throws IOException {
-        N = Integer.parseInt(br.readLine());
-        nums = new long[N + 1];
+    static int N;
+    static long[] cards;
 
-        for (int i = 1; i <= N; i++) {
-            nums[i] = Long.parseLong(br.readLine());
+    public static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        cards = new long[N];
+
+        for (int i = 0; i < N; i++) {
+            cards[i] = Long.parseLong(br.readLine());
         }
+
+        Arrays.sort(cards);
     }
 
-    private static void pro() {
-        // 카드 정렬
-        Arrays.sort(nums, 1, N + 1);
+    private static void pro() throws IOException {
 
-        // 첫번째 카드
-        max = nums[1];
-        maxCnt = 1;
-        currentCnt = 1;
+        int max_cnt = 1;
+        int curr_cnt = 1;
+        long max_value = cards[0];
 
-        // 두번째 카드부터 배열 전체를 돌면서 카운트
-        for (int i = 2; i <= N; i++) {
-            // 이전 값과 같으면
-            if (nums[i] == nums[i - 1]) {
-                // 현재 카운트 증가
-                currentCnt++;
+        for (int i = 1; i < N; i++) {
+            if (cards[i] == cards[i-1]) {
+                curr_cnt++;
 
-                // maxCnt, max 갱신
-                if (maxCnt < currentCnt) {
-                    maxCnt = currentCnt;
-                    max = nums[i];
+                if (max_cnt < curr_cnt) {
+                    max_value = cards[i];
+                    max_cnt = curr_cnt;
                 }
             } else {
-                // 1부터 다시 세기
-                currentCnt = 1;
+                curr_cnt = 1;
             }
-
         }
 
-
+        bw.write(max_value + " ");
     }
 
     public static void main(String[] args) throws Exception {
@@ -55,9 +49,6 @@ public class Main {
 
         pro();
 
-        bw.write(max+" ");
-
-        br.close();
         bw.close();
     }
 }
