7c5b2421e5bda1edfd8ee2552c17dff7caeba39f
이주성 <32183520@dankook.ac.kr>
2024-08-28 16:15:07 +0900

    [Gold IV] Title: 공유기 설치, Time: 288 ms, Memory: 30408 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -3,53 +3,52 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
     static int N, C;
-    static int house[];
+    static int max = Integer.MIN_VALUE;
+    static int[] houses;
 
     public static void init() throws IOException {
-        st = new StringTokenizer(br.readLine(), " ");
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        house = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            house[i] = Integer.parseInt(br.readLine());
+        houses = new int[N];
+        for (int i = 0; i < N; i++) {
+            houses[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, houses[i]);
         }
+
+        Arrays.sort(houses);
     }
 
-    // D 만큼 거리 차이가 난다면 C 만큼의 공유기 설치 가능? -> Yes or No
-    static boolean determination(int D) {
+    private static boolean yesOrNo(int h) {
         int cnt = 1;
-        int last = house[1];
+        int pre = houses[0];
 
-        for (int i = 2; i <= N; i++) {
-            if (house[i] - last >= D) {
-                cnt++;
-                last = house[i];
-            }
+        for (int i = 1; i < N; i++) {
+            if (pre + h > houses[i]) continue;
+
+            cnt++;
+            pre = houses[i];
         }
 
         return cnt >= C;
     }
 
     private static void pro() throws IOException {
-        // 집 정렬하기
-        Arrays.sort(house, 1, N + 1);
+        int start = 0;
+        int end = max;
+        int result = -1;
 
-        // 이분 탐색 범위 1~10억
-        int start = 1;
-        int end = 1000000000; // 10억
-        int result = 0;
-
-        // 이분 탐색으로 마지막 Yes 찾아내기!
         while (start <= end) {
             int mid = (start + end) / 2;
 
-            // Yes일 경우!
-            if (determination(mid)) {
+            if (yesOrNo(mid)) {
                 result = mid;
                 start = mid + 1;
             } else {
@@ -57,8 +56,7 @@ public class Main {
             }
         }
 
-        bw.write(result+" ");
-
+        bw.write(result + " ");
     }
 
     public static void main(String[] args) throws Exception {
@@ -66,7 +64,6 @@ public class Main {
 
         pro();
 
-        br.close();
         bw.close();
     }
 }
