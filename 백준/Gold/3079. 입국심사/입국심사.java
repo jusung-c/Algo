732c0829c8fc215fd4512ff7f507e623a11634c4
이주성 <32183520@dankook.ac.kr>
2024-08-30 14:04:38 +0900

    [Gold V] Title: 입국심사, Time: 284 ms, Memory: 23576 KB -BaekjoonHub

--- 백준/Gold/3079. 입국심사/입국심사.java
+++ 백준/Gold/3079. 입국심사/입국심사.java
@@ -2,73 +2,60 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/*
-    0. 문제
-        - 조건: M명이 N개의 입국심사대에서 심사
-        - 타겟: 걸리는 시간 T
-
-    1. 아이디어
-        - 주어진 시간 T 동안 M명이 N개의 입국심사대를 통과할 수 있는가?
-        -
-
-    2. 시간 복잡도
-
- */
-
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader br;
     static StringTokenizer st;
     static int N, M;
-    static int[] time;
+    static int min = Integer.MAX_VALUE;
+    static int[] times;
 
     public static void init() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        time = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            time[i] = Integer.parseInt(br.readLine());
+        times = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(br.readLine());
+            min = Math.min(min, times[i]);
+        }
+    }
+
+    private static boolean yesOrNo(long k) {
+        long cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            cnt += k / times[i];
+
+            // 오버플로우 신경쓰기
+            if (cnt >= M) return true; 
         }
 
+        return cnt >= M;
     }
 
     private static void pro() throws IOException {
-        long start = Arrays.stream(time).min().getAsInt();
-        long end = (long) Arrays.stream(time).max().getAsInt() * M;
-        long result = 0;
+        long start = min;
+        long end = 1_000_000_000_000_000_000L;
+        long result = -1;
 
         while (start <= end) {
             long mid = (start + end) / 2;
 
             if (yesOrNo(mid)) {
                 result = mid;
-
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
 
-        bw.write(result + "");
+        bw.write(result + " ");
 
-    }
-
-    private static boolean yesOrNo(long T) {
-        long sum = 0;
-
-        for (int i = 1; i <= N; i++) {
-            sum += T / time[i];
-
-            if (sum >= M) {
-                return true;
-            }
-        }
-
-        return sum >= M;
     }
 
     public static void main(String[] args) throws Exception {
