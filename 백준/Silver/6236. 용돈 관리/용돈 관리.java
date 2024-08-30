1b094d577dbbf3aa0e1ef6d49d510811a1f67988
이주성 <32183520@dankook.ac.kr>
2024-08-30 11:25:09 +0900

    [Silver II] Title: 용돈 관리, Time: 232 ms, Memory: 22520 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,78 +1,70 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N, M, sum;
-    static int[] amount;
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int N, M;
+    static int max = Integer.MIN_VALUE;
+    static int[] requests;
 
-    static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        amount = new int[N + 1];
-        sum = 0;
-        for (int i = 1; i <= N; i++) {
-            amount[i] = Integer.parseInt(br.readLine());
-            sum += amount[i];
+        requests = new int[N];
+        for (int i = 0; i < N; i++) {
+            requests[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, requests[i]);
         }
-
     }
 
-    static void pro() throws IOException {
-        // 이분 탐색
-        int L = amount[1];
-        for (int i = 1; i <= N; i++) {
-            L = Math.max(amount[i], L);
-        }
-
-        int R = 1000000000;
-        int ans = 0;
-
-        while (L <= R) {
-            int mid = (L + R) / 2;
-
-            if (YesOrNo(mid)) {
-                ans = mid;
-                R = mid - 1;
-            } else {
-                L = mid + 1;
-            }
-        }
-
-        bw.write(ans+" ");
-    }
-
-    static boolean YesOrNo(int K) {
-        int sum = 0;
+    private static boolean yesOrNo(int k) {
         int cnt = 1;
+        int money = k;
 
-        for (int i = 1; i <= N; i++) {
-            // 금액이 인출한 K보다 클 경우
-            if(sum + amount[i] > K) {
-                // 한번 더 인출
-                cnt++;
-                // 새로 인출했으니 sum 갱신
-                sum = amount[i];
-
-            // K 안에서 해결 가능한 경우
+        for (int i = 0; i < N; i++) {
+            if (requests[i] <= money) {
+                money -= requests[i];
             } else {
-                sum += amount[i];
+                cnt++;
+                money = k - requests[i];
             }
         }
 
         return cnt <= M;
     }
 
-    public static void main(String[] args) throws IOException {
+    private static void pro() throws IOException {
+        int start = max;
+        int end = 1_000_000_000;
+        int result = -1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (yesOrNo(mid)) {
+                end = mid - 1;
+                result = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        bw.write(result + " ");
+
+    }
+
+    public static void main(String[] args) throws Exception {
         init();
 
         pro();
 
         bw.close();
     }
-
 }
