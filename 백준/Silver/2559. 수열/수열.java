77851a8779a8d38802ff9418ae3e3e0e7ea545c4
이주성 <32183520@dankook.ac.kr>
2024-09-03 16:46:20 +0900

    [Silver III] Title: 수열, Time: 268 ms, Memory: 24336 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,47 +1,55 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
     static int N, K;
     static int[] temp;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
         temp = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             temp[i] = Integer.parseInt(st.nextToken());
         }
     }
 
     private static void pro() throws IOException {
-        int R = 0;
-        int result = 0;
-        int day = 1;
-        int ans = Integer.MIN_VALUE;
+        int r = 0;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
 
-        for (int L = 1; L <= N - K + 1; L++) {
-            result -= temp[L - 1];
-            day--;
-
-            while (R + 1 <= N && day < K) {
-                R++;
-                day++;
-                result += temp[R];
-            }
-
-            ans = Math.max(ans, result);
-//            bw.write(ans+" ");
+        for (int i = 1; i < K; i++) {
+            r++;
+            sum += temp[r];
         }
 
-        bw.write(ans + " ");
+        for (int l = 1; l <= N; l++) {
+
+            sum -= temp[l - 1];
+
+            if (r + 1 <= N) {
+                r++;
+                sum += temp[r];
+
+            } else {
+                break;
+            }
+
+            max = Math.max(max, sum);
+        }
+
+        bw.write(max + " ");
+
     }
 
     public static void main(String[] args) throws Exception {
