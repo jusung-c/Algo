0860b5a90ffe1b33d6ecda80d9d926be5f0eca37
이주성 <32183520@dankook.ac.kr>
2024-09-03 11:47:15 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 15520 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,53 +1,55 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
+    static StringTokenizer st;
     static int N, M;
-    static int[] nums;
+    static int[] arr;
 
-    static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        nums = new int[N + 1];
-        st = new StringTokenizer(br.readLine(), " ");
+        arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
     }
 
-    static void pro() throws IOException {
-        int R = 0;
+    private static void pro() throws IOException {
+        int r = 0;
         int sum = 0;
         int ans = 0;
 
-        for (int L = 1; L <= N; L++) {
-            sum -= nums[L - 1];
+        for (int l = 1; l <= N; l++) {
+            sum -= arr[l - 1];
 
-            while (R + 1 <= N && sum < M) {
-                R++;
-                sum += nums[R];
+            while (r + 1 <= N && sum < M) {
+                r++;
+                sum += arr[r];
             }
 
             if (sum == M) {
                 ans++;
             }
-
         }
 
-        bw.write(ans+" ");
+        bw.write(ans + " ");
+
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         init();
 
         pro();
 
         bw.close();
     }
-
 }
