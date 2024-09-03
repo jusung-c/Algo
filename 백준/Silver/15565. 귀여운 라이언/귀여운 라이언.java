3af802f481435e13514cbbc11fb4b2c566985c2e
이주성 <32183520@dankook.ac.kr>
2024-09-03 15:54:14 +0900

    [Silver I] Title: 귀여운 라이언, Time: 384 ms, Memory: 73708 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.java
@@ -1,59 +1,51 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
-/*
-    0. 문제
-
-    1. 아이디어
-
-    2. 시간 복잡도
-
- */
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader br;
     static StringTokenizer st;
     static int N, K;
-    static int[] dolls;
+    static int[] arr;
 
     public static void init() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        dolls = new int[N + 1];
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= N; i++) {
-            dolls[i] = Integer.parseInt(st.nextToken());
-        }
+        arr = new int[N + 1];
 
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
     }
 
     private static void pro() throws IOException {
-        int R = 0;
+        int r = 0;
         int cnt = 0;
-        int size = N + 1;
+        int min = Integer.MAX_VALUE;
 
-        for (int L = 1; L <= N; L++) {
-            if (dolls[L - 1] == 1) {
-                cnt--;
+        for (int l = 1; l <= N; l++) {
+            if (arr[l-1] == 1) cnt--;
+
+            while (r + 1 <= N && cnt < K) {
+                r++;
+                if (arr[r] == 1) cnt++;
             }
 
-            while (R + 1 <= N && cnt < K) {
-                R++;
-                if(dolls[R] == 1) cnt++;
+            if (cnt >= K) {
+                min = Math.min(min, r - l + 1);
             }
-
-            if (cnt >= K) size = Math.min(size, R - L + 1);
         }
 
-        if (size == N + 1) {
+        if (min == Integer.MAX_VALUE) {
             bw.write("-1");
         } else {
-            bw.write(size + " ");
+            bw.write(min + " ");
         }
 
     }
