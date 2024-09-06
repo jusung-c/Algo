b298d2b8e4298df889ef5a73ecc7232e192db612
이주성 <32183520@dankook.ac.kr>
2024-09-07 01:04:26 +0900

    [Gold V] Title: 두 용액, Time: 332 ms, Memory: 28788 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,62 +1,60 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
     static int N;
-    static int[] liquid;
-
+    static int[] liquids;
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        liquid = new int[N + 1];
+        liquids = new int[N + 1];
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            liquid[i] = Integer.parseInt(st.nextToken());
+            liquids[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(liquids, 1, N + 1);
     }
 
     private static void pro() throws IOException {
-        // 1. 용액 정렬
-        Arrays.sort(liquid, 1, N + 1);
+        int A = 0;
+        int B = 0;
 
-        // 2. 투 포인터
-        int L = 1;  // 최소
-        int R = N;  // 최대
-        int r1 = 0;
-        int r2 = 0;
-        int ans = Integer.MAX_VALUE;
 
-        while (L < R) {
-            // 용액 혼합
-            int sum = liquid[L] + liquid[R];
-//            System.out.println(sum+" ");
+        int l = 1;
+        int r = N;
+        int min = Integer.MAX_VALUE;
 
-            if (Math.abs(sum) < ans) {
-                r1 = liquid[L];
-                r2 = liquid[R];
-                ans = Math.abs(sum);
-//                System.out.println(result[0] + " " + result[1]);
+        while (l < r) {
+            int sum = liquids[l] + liquids[r];
+            int sum_abs = Math.abs(sum);
+
+            // 최솟값 갱신
+            if (sum_abs < min) {
+                A = liquids[l];
+                B = liquids[r];
+                min = sum_abs;
             }
 
-            // 0보다 큰 경우
-            if (sum > 0) {
-                R--;
-            }
-
-            // 0보다 작은 경우
-            else {
-                L++;
+            // l의 최선
+            if (sum == 0) {
+                bw.write(A + " " + B);
+                bw.close();
+                System.exit(0);
+            } else if (sum < 0) {
+                l++;
+            } else {
+                r--;
             }
         }
 
-        bw.write(r1 + " " + r2);
-
+        bw.write(A + " " + B);
     }
 
     public static void main(String[] args) throws Exception {
