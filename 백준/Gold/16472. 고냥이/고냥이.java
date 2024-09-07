d89bfa600dd85305ef1395e1395c47cbeedbd9bc
이주성 <32183520@dankook.ac.kr>
2024-09-08 00:18:33 +0900

    [Gold IV] Title: 고냥이, Time: 164 ms, Memory: 16660 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -1,59 +1,60 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
-    static int N, kind;
-    static String s;
-    static int[] cnt;
-    static char[] arr;
+    static int N;
+    static char[] chars;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        s = br.readLine();
-        cnt = new int[26];
-        arr = new char[s.length()];
-
-        for (int i = 0; i < s.length(); i++) {
-            arr[i] = s.charAt(i);
-        }
+        chars = br.readLine().toCharArray();
     }
 
     private static void pro() throws IOException {
-        int ans = 0;
+        int R = -1;
+        int[] cnt = new int[26];
+        int length = Integer.MIN_VALUE;
 
-        for (int L = 0, R = 0; R < s.length(); R++) {
-            // R 번째 문자 추가
-            cnt[arr[R] - 'a']++;
-
-            // 불가능하면 가능할 때까지 L을 이동
-            while (true) {
-                kind = 0;
-                for (int i = 0; i < 26; i++) {
-                    if (cnt[i] != 0) {
-                        kind++;
-                    }
-                }
-
-                if (kind <= N) {
-                    break;
-                }
-
-                // L 문자 삭제 후 포인터 이동
-                cnt[arr[L] - 'a']--;
-                L++;
+        for (int L = 0; L < chars.length; L++) {
+            if (L != 0) {
+                cnt[chars[L-1] - 'a']--;
             }
 
-            // 정답 갱신
-            ans = Math.max(ans, R - L + 1);
+            while (R + 1 < chars.length && checkValid(cnt)) {
+                R++;
+                cnt[chars[R] - 'a']++;
+            }
+
+            if (!checkValid(cnt)) {
+                cnt[chars[R] - 'a']--;
+                R--;
+            }
+
+//            bw.write("L = " + L + " R = " + R + " cnt = " + checkValid(cnt));
+//            bw.newLine();
+
+            if (checkValid(cnt)) {
+                length = Math.max(length, R - L + 1);
+            }
         }
 
-        bw.write(ans+" ");
+        bw.write(length + " ");
+
+    }
+
+    private static boolean checkValid(int[] cnt) {
+        int val = 0;
+
+        for (int i = 0; i < cnt.length; i++) {
+            if (cnt[i] > 0) val++;
+        }
+
+        return val <= N;
     }
 
     public static void main(String[] args) throws Exception {
