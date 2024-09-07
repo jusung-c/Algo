b8ba3d018767ba36737b9e9baa4dcf5f10e3cee4
이주성 <32183520@dankook.ac.kr>
2024-09-08 00:47:04 +0900

    [Silver IV] Title: 주몽, Time: 212 ms, Memory: 18292 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,53 +1,53 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
     static int N, M;
-    static int[] arr;
+    static int[] nums;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        arr = new int[N + 1];
 
-        st = new StringTokenizer(br.readLine(), " ");
+        nums = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
     }
 
     private static void pro() throws IOException {
-        // 배열 정렬하기
-        Arrays.sort(arr, 1, N + 1);
+        Arrays.sort(nums);
 
-        // 투 포인터 사용
-        int ans = 0;
+        // 1 2 3 4 5 7
+
         int L = 1;
         int R = N;
+        int sum = 0;
+        int cnt = 0;
 
         while (L < R) {
-            int sum = arr[L] + arr[R];
+            sum = nums[L] + nums[R];
 
             if (sum == M) {
-                ans++;
+                cnt++;
                 L++;
-            } else if (sum > M) {
                 R--;
-            } else {
+            } else if (sum < M) {
                 L++;
+            } else {
+                R--;
             }
         }
 
-        bw.write(ans + " ");
-
-
+        bw.write(cnt + " ");
     }
 
     public static void main(String[] args) throws Exception {
