a768e9df6cf34de2426625d8ce7e993210dc2ee1
이주성 <32183520@dankook.ac.kr>
2024-09-09 20:19:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 216 ms, Memory: 19944 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,36 +3,37 @@ import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br;
     static StringTokenizer st;
     static int N, M, V;
     static ArrayList<Integer>[] adj;
     static boolean[] visit;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine(), " ");
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         V = Integer.parseInt(st.nextToken());
 
         adj = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            adj[i] = new ArrayList<Integer>();
+            adj[i] = new ArrayList<>();
         }
+
         for (int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            // 양방향 그래프이므로
             adj[x].add(y);
             adj[y].add(x);
         }
+
     }
 
-    // x를 갈 수 있다는 걸 알고 방문한 상태
     static void dfs(int x) throws IOException {
-        // 방문 후 출력
         visit[x] = true;
         bw.write(x + " ");
 
@@ -41,8 +42,7 @@ public class Main {
             dfs(y);
         }
     }
-
-    // start에서 시작해서 갈 수 있는 정점들을 모두 탐색
+    
     static void bfs(int x) throws IOException {
         Queue<Integer> que = new LinkedList<>();
 
@@ -63,23 +63,18 @@ public class Main {
     }
 
     private static void pro() throws IOException {
-        // 모든 정점마다 정렬
         for (int i = 1; i <= N; i++) {
             Collections.sort(adj[i]);
         }
 
         visit = new boolean[N + 1];
-
         dfs(V);
 
-        bw.write("\n");
+        bw.newLine();
 
-        // BFS를 위한 visit 배열 초기화
-        for (int i = 1; i <= N; i++) {
-            visit[i] = false;
-        }
-
+        visit = new boolean[N + 1];
         bfs(V);
+
     }
 
     public static void main(String[] args) throws Exception {
