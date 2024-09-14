8195b4e8a2ee19e23e64fa37afd2451fcd308e55
이주성 <32183520@dankook.ac.kr>
2024-09-14 17:43:37 +0900

    [Silver I] Title: 경로 찾기, Time: 256 ms, Memory: 32552 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,74 +1,59 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
     static int N;
-    static int[][] map, ans;
+    static ArrayList<Integer>[] adj;
     static boolean[] visit;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        map = new int[N + 1][N + 1];
-        ans = new int[N + 1][N + 1];
-        visit = new boolean[N + 1];
-
-        // map 저장
+        adj = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                int n = Integer.parseInt(st.nextToken());
+                if (n == 1) adj[i].add(j);
             }
         }
     }
 
     private static void pro() throws IOException {
-        for (int x = 1; x <= N; x++) {
-            bfs(x);
-        }
-
-        // ans 출력
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                bw.write(ans[i][j] + " ");
+                visit = new boolean[N + 1];
+                int result = isExistPath(i, j) ? 1 : 0;
+                bw.write(result + " ");
             }
-            bw.write("\n");
+            bw.newLine();
         }
     }
 
-    private static void bfs(int start) {
+    private static boolean isExistPath(int i, int j) {
         Queue<Integer> que = new LinkedList<>();
-
-        for (int i = 1; i <= N; i++) {
-            visit[i] = false;
-        }
-
-        que.add(start);
-        visit[start] = false;
+        que.add(i);
+        visit[i] = true;
 
         while (!que.isEmpty()) {
-            int x = que.poll();
+            i = que.poll();
 
-            for (int y = 1; y <= N; y++) {
-                if (map[x][y] == 0) continue;
+            for (int y : adj[i]) {
+                if (y == j) return true;
                 if (visit[y]) continue;
 
                 que.add(y);
                 visit[y] = true;
-                ans[start][y] = 1;
             }
         }
 
-//        for (int i = 1; i <= N; i++) {
-//            if (visit[i]) ans[x][i] = 1;
-//            else ans[x][i] = 0;
-//        }
-
+        return false;
     }
 
     public static void main(String[] args) throws Exception {
