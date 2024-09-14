ab9416d16606229df70c66b2f361168044fa7b32
이주성 <32183520@dankook.ac.kr>
2024-09-14 17:17:25 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 15848 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,74 +1,61 @@
-// 언어 : JAVA , (성공/실패) : 1/0 
-// 메모리 : 18652 KB , 시간 : 396 ms
-
-/*
-
-// 1. 아이디어
-1번 컴퓨터부터 BFS
-
-// 2. 시간 복잡도
-O(V+E)
-V : 100
-E : 100*100
-
-O(V+E) = O(10100) < 1초
-
-// 3. 자료구조
-큐 (BFS)
-간선 정보 graph
-방문 기록 visited
-*/
-
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int V, E;
-    static Queue<Integer> q = new LinkedList<>();
-    static int[][] graph;
-    static int cnt;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+    static StringTokenizer st;
+    static int N, K, cnt;
+    static ArrayList<Integer>[] adj;
+    static boolean[] visit;
 
-        V = Integer.parseInt(br.readLine());
-        E = Integer.parseInt(br.readLine());
+    public static void init() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+        visit = new boolean[N + 1];
+        cnt = 0;
 
-        graph = new int[V+1][V+1];
-
-        StringTokenizer st;
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            graph[a][b] = graph[b][a] = 1;
+        adj = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            adj[i] = new ArrayList<>();
         }
 
-        BFS(1);
+        for (int i = 1; i <= K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-        bw.write((cnt-1)+" ");
-        bw.close();
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+
+        dfs(1);
+
+        bw.write(cnt - 1 + " ");
     }
 
-    static void BFS(int i) {
-        boolean[] visited = new boolean[V + 1];
+    private static void dfs(int x) {
+        visit[x] = true;
+        cnt++;
 
-        visited[i] = true;
-        q.offer(i);
-
-        while (!q.isEmpty()) {
-            int curr = q.remove();
-            cnt++;
-//            System.out.println(curr+" ");
-
-            for(int next=1; next<=V; next++) {
-                if(!visited[next] && graph[curr][next] !=0) {
-                    visited[next] = true;
-                    q.add(next);
-                }
-            }
+        for (int y : adj[x]) {
+            if (visit[y]) continue;
+            dfs(y);
         }
+    }
+
+    private static void pro() throws IOException {
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        init();
+
+        pro();
+
+        bw.close();
     }
 }
