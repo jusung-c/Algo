c45d23a6aea8cd23077e93c89c08876d67382108
이주성 <32183520@dankook.ac.kr>
2024-09-14 16:09:57 +0900

    [Silver II] Title: 섬의 개수, Time: 156 ms, Memory: 16536 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,81 +1,73 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
-    static int w, h;
-    static boolean[][] visit;
-    static int[][] dir = {
-            {1, 0}, {0, 1},
-            {-1, 0}, {0, -1},
-            {1, 1}, {-1, -1},
-            {1, -1}, {-1, 1}
-    };
+    static int w, h, cnt;
     static int[][] map;
+    static boolean[][] visit;
+    static int[][] dir = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0},
+            {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
 
     public static void init() throws IOException {
-        map = new int[w + 1][h + 1];
-        visit = new boolean[w + 1][h + 1];
-
-        for (int i = 1; i <= h; i++) {
+        while (true) {
             st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= w; j++) {
-                int a = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            cnt = 0;
 
-                map[j][i] = a;
+            if (w == 0 || h == 0) break;
+
+            map = new int[h][w];
+            visit = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
             }
-        }
 
-        pro();
+            pro();
+
+            bw.write(cnt + " ");
+            bw.newLine();
+        }
     }
 
     private static void pro() throws IOException {
-        int ans = 0;
-        for (int x = 1; x <= w; x++) {
-            for (int y = 1; y <= h; y++) {
-                if (map[x][y] == 0) continue;
-                if (visit[x][y]) continue;
-
-                dfs(x, y);
-                ans++;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (map[i][j] == 1 && !visit[i][j]) {
+                    cnt++;
+                    dfs(i, j);
+                }
             }
         }
-
-        bw.write(ans+"\n");
 
     }
 
     private static void dfs(int x, int y) {
         visit[x][y] = true;
 
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dir[i][0];
-            int ny = y + dir[i][1];
+        for (int d = 0; d < 8; d++) {
+            int nx = x + dir[d][0];
+            int ny = y + dir[d][1];
 
-            if (nx < 1 || ny < 1 || nx > w || ny > h) continue;
-            if (visit[nx][ny]) continue;
+            if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
             if (map[nx][ny] == 0) continue;
-            dfs(nx, ny);
+            if (visit[nx][ny]) continue;
 
+            dfs(nx, ny);
         }
     }
 
     public static void main(String[] args) throws Exception {
-
-        while (true) {
-            st = new StringTokenizer(br.readLine(), " ");
-            w = Integer.parseInt(st.nextToken());
-            h = Integer.parseInt(st.nextToken());
-
-            if (w == 0 && h == 0) {
-                break;
-            }
-
-            init();
-        }
+        init();
 
         bw.close();
     }
