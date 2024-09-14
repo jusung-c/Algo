58bef2a5bf76f446c16506e198350de7f5bff023
이주성 <32183520@dankook.ac.kr>
2024-09-14 16:15:35 +0900

    [Silver II] Title: 섬의 개수, Time: 156 ms, Memory: 16492 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,7 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,6 +10,15 @@ public class Main {
     static boolean[][] visit;
     static int[][] dir = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0},
             {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void init() throws IOException {
         while (true) {
@@ -44,11 +51,35 @@ public class Main {
             for (int j = 0; j < w; j++) {
                 if (map[i][j] == 1 && !visit[i][j]) {
                     cnt++;
-                    dfs(i, j);
+//                    dfs(i, j);
+                    bfs(i, j);
                 }
             }
         }
 
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<Point> que = new LinkedList<>();
+
+        que.add(new Point(x, y));
+        visit[x][y] = true;
+
+        while (!que.isEmpty()) {
+            Point p = que.poll();
+
+            for (int d = 0; d < 8; d++) {
+                int nx = p.x + dir[d][0];
+                int ny = p.y + dir[d][1];
+
+                if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+                if (visit[nx][ny]) continue;
+                if (map[nx][ny] == 0) continue;
+
+                que.add(new Point(nx, ny));
+                visit[nx][ny] = true;
+            }
+        }
     }
 
     private static void dfs(int x, int y) {
