7c2c48f4592953ebf8fe7ec43520abab680498f4
이주성 <32183520@dankook.ac.kr>
2024-09-19 10:54:36 +0900

    [Gold IV] Title: 연구소, Time: 480 ms, Memory: 130668 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,21 +1,17 @@
-import javax.swing.*;
-import java.awt.*;
 import java.io.*;
 import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
-    static int N, M, ans, B;
-    static int[][] map, blank;
-    static int[][] dir = new int[][]{
-            {1, 0}, {0, 1},
-            {-1, 0}, {0, -1}
-    };
+    static int N, M, ans, space;
+    static int[][] map;
+    static Point[] blank;
     static boolean[][] visit;
+    static int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     static class Point {
-        int x;
-        int y;
+        int x, y;
 
         public Point(int x, int y) {
             this.x = x;
@@ -24,110 +20,100 @@ public class Main {
     }
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        ans = -1;
-        B = 0;
+        ans = 0;
+        space = 0;
 
-        map = new int[N + 1][M + 1];
-        visit = new boolean[N + 1][M + 1];
-        blank = new int[N * M + 1][2];
-
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= M; j++) {
+        visit = new boolean[N][M];
+        map = new int[N][M];
+        blank = new Point[N * M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+
+                // 벽을 세울 수 있는 좌표 저장
+                if (map[i][j] == 0) {
+                    blank[space] = new Point(i, j);
+                    space++;
+                };
             }
         }
-    }
-
-    static void bfs() {
-        Queue<Point> Q = new LinkedList<>();
-
-        // 방문 리스트 초기화
-        for (int i = 1; i <= N; i++)
-            for (int j = 1; j <= M; j++) visit[i][j] = false;
-
-        // 바이러스 시작점들 큐에 넣기
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (map[i][j] == 2) {
-                    Q.add(new Point(i, j));
-                    visit[i][j] = true;
-                }
-            }
-        }
-
-        // BFS 진행
-        while (!Q.isEmpty()) {
-            Point p = Q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = p.x + dir[i][0];
-                int ny = p.y + dir[i][1];
-
-                if (nx<1 || ny<1 || nx>N || ny>M) continue;
-                if (visit[nx][ny]) continue;
-                if (map[nx][ny] != 0) continue;
-
-                visit[nx][ny] = true;
-                Q.add(new Point(nx, ny));
-            }
-        }
-
-        // 전염 완료 시 안전 구역 계산 후 정답 갱신
-        int cnt = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (map[i][j] == 0 && !visit[i][j]) cnt++;
-            }
-        }
-        ans = Math.max(ans, cnt);
     }
 
     private static void pro() throws IOException {
-        // 벽을 세울 수 있는 빈칸의 개수 세기
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-                if (map[i][j] == 0) {
-                    B++;
-                    blank[B][0] = i;
-                    blank[B][1] = j;
-                }
-            }
-        }
+        dfs(0, 0);
 
-        dfs(1, 0);
-
-        // 정답 출력
         bw.write(ans + " ");
-
     }
 
-    static void dfs(int idx, int wall_cnt) {
-        // idx 번째 빈칸 위키 정보
-        int x = blank[idx][0];
-        int y = blank[idx][1];
-
-        // 벽 3개를 모두 세웠을 경우
-        if (wall_cnt == 3) {
+    static void dfs(int idx, int selected_cnt) {
+        if (selected_cnt == 3) {
             // 바이러스 퍼뜨리기
             bfs();
             return;
         }
 
         // 더 이상 세울 수 있는 벽이 없는 경우
-        if (idx > B) return;
+        if (idx >= space) return;
 
-        // idx에 벽을 세울 경우
-        map[x][y] = 1;
-        dfs(idx + 1, wall_cnt + 1);
+        // idx에 벽을 세우는 경우
+        map[blank[idx].x][blank[idx].y] = 1;
+        // 다음 벽 세우러 ㄱㄱ
+        dfs(idx + 1, selected_cnt + 1);
 
-        // idx에 벽을 세우지 않을 경우
-        map[x][y] = 0;
-        dfs(idx + 1, wall_cnt);
+        // idx에 벽을 안 세우는 경우
+        map[blank[idx].x][blank[idx].y] = 0;
+        // 다음 벽 세우러 ㄱㄱ
+        dfs(idx + 1, selected_cnt);
+
+    }
+
+    private static void bfs() {
+        Queue<Point> que = new LinkedList<>();
+
+        // 모든 바이러스들을 시작점으로 넣기
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                // 방문 배열 초기화
+                visit[i][j] = false;
+
+                if (map[i][j] == 2) {
+                    que.add(new Point(i, j));
+                    visit[i][j] = true;
+                }
+            }
+        }
+
+        // 바이러스 전염 시작
+        while (!que.isEmpty()) {
+            Point p = que.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int nx = p.x + dir[d][0];
+                int ny = p.y + dir[d][1];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (map[nx][ny] != 0) continue;
+                if (visit[nx][ny]) continue;
+
+                que.add(new Point(nx, ny));
+                visit[nx][ny] = true;
+            }
+        }
+
+        // 전염 완료 후 안전 영역 크기 계산
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                // 이 때 전염된 구역을 제외하기 위해 방문처리 여부를 확인해줘야 한다.
+                if (map[i][j] == 0 && !visit[i][j]) cnt++;
+            }
+        }
+
+        ans = Math.max(ans, cnt);
     }
 
     public static void main(String[] args) throws Exception {
