0291a460d51cf50485b1ef79f11f7c7892c36479
이주성 <32183520@dankook.ac.kr>
2024-09-21 14:42:49 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 21684 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,62 +1,60 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
-    static int start, end;
-    static int MAX = 100001;
-    static int[] dist = new int[MAX];
+    static int N, K;
+    static int[] dist;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
+        dist = new int[100001];
+        for (int i = 0; i < 100001; i++) {
+            dist[i] = Integer.MAX_VALUE;
+        }
     }
 
     private static void pro() throws IOException {
-//        if (start == end) {
-//            bw.write(0 + " ");
-//            return;
-//        } else {
-//            bfs();
-//        }
-        bfs();
+        bfs(N);
 
-        bw.write(dist[end] - 1 + " ");
-
+        bw.write(dist[K] + " ");
     }
 
-    private static void bfs() {
-        Queue<Integer> Q = new LinkedList<>();
+    private static void bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
+        dist[start] = 0;
 
-        Q.add(start);
-        dist[start] = 1;
+        while (!que.isEmpty()) {
+            int p = que.poll();
 
-        while (!Q.isEmpty()) {
-            int n = Q.poll();
+            for (int i = 0; i < 3; i++) {
+                int np = -1;
 
-            for (int next = 0; next < 3; next++) {
-                int nn;
-
-                if (next == 0) {
-                    nn = n + 1;
-                } else if (next == 1) {
-                    nn = n - 1;
-                } else {
-                    nn = n * 2;
+                switch (i) {
+                    case 0:
+                        np = p + 1;
+                        break;
+                    case 1:
+                        np = p - 1;
+                        break;
+                    case 2:
+                        np = p * 2;
+                        break;
                 }
 
-                if (nn >= 0 && nn < MAX && dist[nn] == 0) {
-                    Q.add(nn);
-                    dist[nn] = dist[n] + 1;
-                }
+                if (np < 0 || np > 100000) continue;
+                if (dist[np] <= dist[p] + 1) continue;
 
+                dist[np] = dist[p] + 1;
+                que.add(np);
             }
+
         }
     }
 
