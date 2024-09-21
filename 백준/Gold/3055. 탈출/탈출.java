7070ef3713aa125335437a420a3bda0ece2970e3
이주성 <32183520@dankook.ac.kr>
2024-09-21 20:14:46 +0900

    [Gold IV] Title: 탈출, Time: 112 ms, Memory: 15120 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,131 +1,141 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    ;
     static StringTokenizer st;
-    static int N, M;
-    static int[][] dist_water, dist_hedgehog;
-    static String[] a;
-    static int[][] dir = new int[][]{
-            {1, 0}, {0, 1},
-            {-1, 0}, {0, -1}
-    };
+    static int R, C;
+    static Point S, D;
+    static int[][] wave, hog;
+    static String[] input;
+    static int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        dist_water = new int[N][M];
-        dist_hedgehog = new int[N][M];
-
-        a = new String[N];
-
-        for (int i = 0; i < N; i++) {
-            a[i] = br.readLine();
+        wave = new int[R][C];
+        hog = new int[R][C];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                wave[i][j] = -1;
+                hog[i][j] = -1;
+            }
         }
 
+        input = new String[R];
+        for (int i = 0; i < R; i++) {
+            input[i] = br.readLine();
+        }
     }
 
     private static void pro() throws IOException {
-        // 물 찾아서 BFS 돌리기
-        bfs_water();
+        // 물이 잠기는 시간 계산하기
+        waveBfs();
 
-        // 고슴도치 탈출 시도
-        bfs_hedgehog();
+//        bw.write("웨이브 정보 \n");
+//        for (int i = 0; i < R; i++) {
+//            for (int j = 0; j < C; j++) {
+//                bw.write(wave[i][j] + " ");
+//            }
+//            bw.newLine();
+//        }
+//        bw.newLine();
 
-        // 정답 출력
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (a[i].charAt(j) == 'D' ) {
-                    if (dist_hedgehog[i][j] == -1) {
-                        bw.write("KAKTUS" + " ");
-                    } else {
-                        bw.write(dist_hedgehog[i][j] + " ");
-                    }
-                }
+        // 고슴도치 시작점 넣기
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (input[i].charAt(j) == 'S') S = new Point(i, j);
+                if (input[i].charAt(j) == 'D') D = new Point(i, j);
             }
         }
+
+        // 고슴도치 대피 경로 탐색
+        hogBfs(S);
+//
+//        bw.write("고슴도치 정보 \n");
+//        for (int i = 0; i < R; i++) {
+//            for (int j = 0; j < C; j++) {
+//                bw.write(dist[i][j] + " ");
+//            }
+//            bw.newLine();
+//        }
+//        bw.newLine();
+
+        if (hog[D.x][D.y] == -1) bw.write("KAKTUS");
+        else bw.write(hog[D.x][D.y] + " ");
     }
 
-    private static void bfs_hedgehog() throws IOException {
-        Queue<Integer> Q = new LinkedList<>();
+    private static void waveBfs() {
+        Queue<Point> que = new LinkedList<>();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dist_hedgehog[i][j] = -1;
-
-                if (a[i].charAt(j) == 'S' ) {
-                    Q.add(i);
-                    Q.add(j);
-
-                    dist_hedgehog[i][j] = 0;
+        // 모든 물을 시작점으로 넣기
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (input[i].charAt(j) == '*') {
+                    que.add(new Point(i, j));
+                    wave[i][j] = 0;
                 }
             }
         }
 
-        // BFS 시작
-        while (!Q.isEmpty()) {
-            int x = Q.poll();
-            int y = Q.poll();
+        while (!que.isEmpty()) {
+            Point p = que.poll();
 
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
+            // 가능한 모든 경로 BFS 탐색
+            for (int d = 0; d < 4; d++) {
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (a[nx].charAt(ny) != '.' && a[nx].charAt(ny) != 'D' ) continue;
-                if (dist_water[nx][ny] != -1 && dist_water[nx][ny] <= dist_hedgehog[x][y] + 1)
-                    continue;  // 물에 잠기지는 않는가?
-                if (dist_hedgehog[nx][ny] != -1) continue;
+                int nx = p.x + dir[d][0];
+                int ny = p.y + dir[d][1];
 
-                Q.add(nx);
-                Q.add(ny);
+                if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+                if (wave[nx][ny] != -1) continue;
+                if (input[nx].charAt(ny) != '.') continue;
 
-                dist_hedgehog[nx][ny] = dist_hedgehog[x][y] + 1;
+                que.add(new Point(nx, ny));
+                wave[nx][ny] = wave[p.x][p.y] + 1;
             }
         }
+
     }
 
-    private static void bfs_water() {
-        Queue<Integer> Q = new LinkedList<>();
+    private static void hogBfs(Point start) {
+        Queue<Point> que = new LinkedList<>();
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dist_water[i][j] = -1;
+        que.add(start);
+        hog[start.x][start.y] = 0;
 
-                if (a[i].charAt(j) == '*' ) {
-                    Q.add(i);
-                    Q.add(j);
+        while (!que.isEmpty()) {
+            Point p = que.poll();
+            for (int d = 0; d < 4; d++) {
+                int nx = p.x + dir[d][0];
+                int ny = p.y + dir[d][1];
 
-                    dist_water[i][j] = 0;
-                }
+                if (nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
+                if (hog[nx][ny] != -1) continue;
+
+                // 이동할 수 없는 지역이면 제외
+                if (input[nx].charAt(ny) != '.' && input[nx].charAt(ny) != 'D') continue;
+
+                // 물이 잠길 예정이면서 도착지점이 아닌 경우 제외 -> 물에 잠길 예정이어도 도착지점이면 이동 가능
+                if (hog[p.x][p.y] + 1>= wave[nx][ny] && wave[nx][ny] != -1) continue;
+
+                que.add(new Point(nx, ny));
+                hog[nx][ny] = hog[p.x][p.y] + 1;
             }
-        }
 
-        // BFS 시작
-        while (!Q.isEmpty()) {
-            int x = Q.poll();
-            int y = Q.poll();
-
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1];
-
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-                if (a[nx].charAt(ny) != '.' ) continue;
-                if (dist_water[nx][ny] != -1) continue;
-
-                Q.add(nx);
-                Q.add(ny);
-                dist_water[nx][ny] = dist_water[x][y] + 1;
-            }
         }
     }
 
