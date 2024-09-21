2c5130032edc672bb85ed21fdebe56624b9aac13
이주성 <32183520@dankook.ac.kr>
2024-09-21 15:09:49 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 128 ms, Memory: 14944 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -1,84 +1,77 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
-    static int N, M;
+    static int N, M, min, ans;
+    static ArrayList<Integer>[] adj;
     static int[] dist;
-    static int[] ans;
-    static ArrayList<Integer>[] friend;
 
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        min = Integer.MAX_VALUE;
+        ans = 0;
 
-        dist = new int[N + 1];
-        friend = new ArrayList[N + 1];
-
+        adj = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
-            friend[i] = new ArrayList<>();
+            adj[i] = new ArrayList<>();
         }
 
-        for (int i = 1; i <= M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+        dist = new int[N + 1];
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-            friend[a].add(b);
-            friend[b].add(a);
+            adj[x].add(y);
+            adj[y].add(x);
         }
     }
 
     private static void pro() throws IOException {
+        for (int i = 1; i <= N; i++) {
+            // dist 배열 초기화
+            for (int j = 1; j <= N; j++) {
+                dist[j] = -1;
+            }
 
-        int r_value = bfs(1);
-        int r_idx = 1;
+            bfs(i);
 
-        for (int i = 2; i <= N; i++) {
-            int v = bfs(i);
-            if (r_value > v) {
-                r_value = v;
-                r_idx = i;
+            int sum = 0;
+            for (int j = 1; j <= N; j++) {
+                sum += dist[j];
+            }
+
+            // 최솟값 갱신
+            if (min > sum) {
+                min = sum;
+                ans = i;
             }
         }
 
-        bw.write(r_idx + " ");
-
+        bw.write(ans + " ");
     }
 
-    private static int bfs(int a) {
-        for (int i = 1; i <= N; i++) {
-            dist[i] = -1;
-        }
+    private static void bfs(int start) {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(start);
+        dist[start] = 0;
 
-        Queue<Integer> Q = new LinkedList<>();
-        Q.add(a);
-        dist[a] = 0;
+        while (!que.isEmpty()) {
+            int x = que.poll();
 
-        int ans = 0;
+            for (int y : adj[x]) {
+                if (dist[y] != -1) continue;
 
-        while (!Q.isEmpty()) {
-            int x = Q.poll();
-            ans += dist[x];
-
-            for (int i = 0; i < friend[x].size(); i++) {
-                int nx = friend[x].get(i);
-
-                if (dist[nx] != -1) continue;
-                Q.add(nx);
-                dist[nx] = dist[x] + 1;
+                que.add(y);
+                dist[y] = dist[x] + 1;
             }
         }
-
-        return ans;
     }
 
     public static void main(String[] args) throws Exception {
