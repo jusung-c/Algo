86bfdee7ba28620f305de8b0940ed15b0be4d7f0
이주성 <32183520@dankook.ac.kr>
2024-09-24 05:08:41 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 512 ms, Memory: 19924 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.java
@@ -1,12 +1,11 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
     static Node root;
-
     static class Node {
         int num;
 
@@ -16,50 +15,39 @@ public class Main {
             this.num = num;
         }
 
-        public Node(int num, Node left, Node right) {
-            this.num = num;
-            this.left = left;
-            this.right = right;
-        }
-
-        public void insert(int n) {
+        public void insertNode(int n) {
             if (n < this.num) {
                 if (this.left == null) this.left = new Node(n);
-                else this.left.insert(n);
+                else this.left.insertNode(n);
             } else {
                 if (this.right == null) this.right = new Node(n);
-                else this.right.insert(n);
+                else this.right.insertNode(n);
             }
-
         }
     }
 
-
     public static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         root = new Node(Integer.parseInt(br.readLine()));
 
         String input;
+        while ((input = br.readLine()) != null && !input.equals("")) {
 
-        while (true) {
-            input = br.readLine();
-            if (input == null || input.equals("")) break;
-            root.insert(Integer.parseInt(input));
+//            System.out.println(Integer.parseInt(input));
+            root.insertNode(Integer.parseInt(input));
         }
     }
 
-    private static void postOrder(Node node) throws IOException {
-        if (node == null) return;
-
-        postOrder(node.left);
-        postOrder(node.right);
-        bw.write(node.num + "\n");
+    private static void pro() throws IOException {
+        backDfs(root);
     }
 
-    private static void pro() throws IOException {
-        postOrder(root);
+    private static void backDfs(Node node) throws IOException {
+        if (node == null) return;
 
+        backDfs(node.left);
+        backDfs(node.right);
+        bw.write(node.num +" ");
+        bw.newLine();
     }
 
     public static void main(String[] args) throws Exception {
