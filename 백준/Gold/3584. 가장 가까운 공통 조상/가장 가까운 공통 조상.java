a0e5738ae65907c3f53f8dc9e8f50c6f93eb2567
이주성 <32183520@dankook.ac.kr>
2024-09-26 01:03:10 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 220 ms, Memory: 24740 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -2,99 +2,64 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int T, N, answer, A, B;
-    static ArrayList<Integer>[] adj;
-    static ArrayList<Integer> A_order, B_order;
-    static boolean[] isChild, visit;
+    static int T, N;
+    static ArrayList<Integer>[] tree;
+    static int[] parent;
+    static boolean[] visited;
 
-    public static void init() throws IOException {
+    public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
 
         for (int t = 0; t < T; t++) {
             N = Integer.parseInt(br.readLine());
-            answer = 0;
 
-            adj = new ArrayList[N + 1];
+            // 트리 초기화
+            tree = new ArrayList[N + 1];
+            parent = new int[N + 1];
+            visited = new boolean[N + 1];
+
             for (int i = 1; i <= N; i++) {
-                adj[i] = new ArrayList<>();
+                tree[i] = new ArrayList<>();
             }
 
-            isChild = new boolean[N + 1];
-            visit = new boolean[N + 1];
-
+            // 간선 정보 입력
             for (int i = 1; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                int parent = Integer.parseInt(st.nextToken());
-                int child = Integer.parseInt(st.nextToken());
-
-                adj[child].add(parent);
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                parent[v] = u;  // 자식 -> 부모 관계 저장
             }
 
+            // 두 노드 입력
             st = new StringTokenizer(br.readLine());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
 
-            pro();
+            // 공통 조상 찾기
+            System.out.println(findLCA(node1, node2));
         }
     }
 
-    private static void pro() throws IOException {
-        A_order = new ArrayList<>();
-        B_order = new ArrayList<>();
+    // 공통 조상을 찾는 함수
+    static int findLCA(int a, int b) {
+        Arrays.fill(visited, false);
 
-        findRoot(A, A_order);
-        findRoot(B, B_order);
-
-//        for (int x : A_order) {
-//            bw.write(x + " ");
-//        }
-//        bw.newLine();
-//        for (int x : B_order) {
-//            bw.write(x + " ");
-//        }
-//        bw.newLine();
-
-        int a_index = A_order.size();
-        int b_index = B_order.size();
-
-        while (true) {
-            a_index--;
-            b_index--;
-
-            if (a_index < 0 || b_index < 0) break;
-
-            int a = A_order.get(a_index);
-            int b = B_order.get(b_index);
-
-            if (a == b) {
-                answer = a;
-                continue;
-            }
-            if (a != b) {
-                break;
-            }
+        // a에서 루트까지 경로 따라가며 방문 처리
+        while (a != 0) {
+            visited[a] = true;
+            a = parent[a];
         }
 
-        bw.write(answer + " ");
-        bw.newLine();
+        // b에서 루트까지 올라가며, 방문된 노드를 찾으면 그게 공통 조상
+        while (b != 0) {
+            if (visited[b]) {
+                return b;
+            }
+            b = parent[b];
+        }
 
-    }
-
-    private static void findRoot(int x, ArrayList<Integer> order) {
-        order.add(x);
-
-        if (adj[x].isEmpty()) return;
-
-        x = adj[x].get(0);
-        findRoot(x, order);
-    }
-
-    public static void main(String[] args) throws Exception {
-        init();
-
-        bw.close();
+        return -1;  // 예외 처리 (이론적으로는 여기에 도달하지 않음)
     }
 }
