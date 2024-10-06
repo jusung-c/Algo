ea73e38bf2a246c2a7a5d1ae2dfd99d87dd5e233
이주성 <32183520@dankook.ac.kr>
2024-10-06 17:34:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,31 +1,24 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public String solution(String s) {
         StringBuilder sb = new StringBuilder();
-        boolean isUpper = true;
         
-        // 문자열의 모든 문자에 대해 반복해서 변환을 시도한 후 이어 붙인다.
-        for (char c : s.toCharArray()) {
-            // 공백일 경우
+        int index = 0;
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            
             if (c == ' ') {
-                // 그대로 이어붙이기
+                index = 0;
                 sb.append(c);
-                
-                // 공백의 다음은 무조건 대문자
-                isUpper = true;
+                continue;
             }
             
-            // 공백이 아닐 경우
-            else {
-                // isUpper에 따라 변환
-                sb.append(isUpper ? Character.toUpperCase(c) : Character.toLowerCase(c));
-                
-                // 다음을 위해 반점
-                isUpper = !isUpper;
-            }
+            sb.append(index % 2 == 0 ? 
+                      Character.toUpperCase(c) : Character.toLowerCase(c));
+            index++;
         }
-
+        
+        // System.out.println(sb);
+        
         return sb.toString();
     }
 }
