b5bb650b043e911294f59a5e817a7511f44a8dc9
이주성 <32183520@dankook.ac.kr>
2024-10-06 16:28:51 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,20 +1,14 @@
 class Solution {
     public int solution(int n) {
-        // 1. n을 3진법으로 표현
-        String s = Integer.toString(n, 3);
-
-        System.out.println("s = " + s);
-
-        // 2. 문자열 뒤집기
-        s = new StringBuilder(s).reverse().toString();
-
-        System.out.println("s = " + s);
-
-        // 3. 10진법으로 표현
-        int answer = Integer.parseInt(s, 3);
-
-        System.out.println("answer = " + answer);
         
-        return answer;
+        // 1. 3진법으로 변환
+        String s = Integer.toString(n, 3);
+        
+        // 2. 뒤집기
+        s = new StringBuilder(s).reverse().toString();
+        
+        // 3. 10진법으로 변환
+        return Integer.parseInt(s, 3);
+        
     }
 }
