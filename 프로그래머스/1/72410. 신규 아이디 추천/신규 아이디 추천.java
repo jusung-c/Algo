48ec5dfda6e6bf530c9d9132db52432ff955d70d
이주성 <32183520@dankook.ac.kr>
2024-10-06 17:26:03 +0900

    [level 1] Title: 신규 아이디 추천, Time: 6.06 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,29 +1,25 @@
 class Solution {
     public String solution(String new_id) {
+        String answer = "";
 
-        // step 1
-        new_id = new_id.toLowerCase();
-
-        // step 2
-        new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
-
-        // step 3
-        new_id = new_id.replaceAll("[.]+", ".");
-
-        // step 4
-        new_id = new_id.replaceAll("^[.]|[.]$", "");
-
-        // step 5
-        new_id = new_id.isEmpty() ? "a" : new_id;
-
-        // step 6
-        new_id = new_id.length() >= 16 ? new_id.substring(0, 15).replaceAll("[.]$", "") : new_id;
-
-        // step 7
-        while (new_id.length() <= 2) {
-            new_id += new_id.charAt(new_id.length() - 1);
+        answer = new_id.toLowerCase();
+        answer = answer.replaceAll("[^a-z0-9.\\-_]", "");
+        answer = answer.replaceAll("[.]+", ".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+        answer = answer.isEmpty() ? "a" : answer;
+        answer = answer.length() >= 16 ? answer.substring(0, 15) : answer;
+        answer = answer.replaceAll("[.]$", "");
+        if (answer.length() <= 2) {
+            while (answer.length() < 3) {
+                answer += String.valueOf(answer.charAt(answer.length() - 1));
+            }
         }
 
-        return new_id;
+        
+        
+        
+        System.out.println(answer);
+        
+        return answer;
     }
 }
