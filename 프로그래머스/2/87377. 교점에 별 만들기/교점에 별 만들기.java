7db7a381dc2121168f0b734210c64602fe83938d
이주성 <32183520@dankook.ac.kr>
2024-10-07 13:27:36 +0900

    [level 2] Title: 교점에 별 만들기, Time: 137.90 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,104 +1,93 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
+// 주어진 n개 직선들의 교점 중 정수 좌표에 별을 그려라
+// 별은 "*"로 빈공간은 "."으로 표현하고, 모든 별을 포함하는 최소한의 크기로 나타내라
+// 직선은 Ax + By + C = 0 형태이며, A, B, C가 직선별로 이차원 배열로 주어진다.
 class Solution {
-    long maxX = Long.MIN_VALUE;
-    long minX = Long.MAX_VALUE;
-    long maxY = Long.MIN_VALUE;
-    long minY = Long.MAX_VALUE;
-
-    class Point {
+    static class Pair {
         long x, y;
-
-        public Point(long x, long y) {
+        
+        public Pair(long x, long y) {
             this.x = x;
             this.y = y;
         }
     }
-
+    
     public String[] solution(int[][] line) {
-        List<Point> pointList = new ArrayList<>();
-
-        // 1. 모든 직선에 대해 정수인 교점을 구한다.
-        for (int i=0; i < line.length; i++) {
-            for (int j = i + 1; j < line.length; j++) {
-                Point p = getPoint(line[i][0], line[i][1], line[i][2],
-                        line[j][0], line[j][1], line[j][2]);
-
-                if (p != null) pointList.add(p);
-            }
+        
+        // 주어진 직선 정보를 2중 for문으로 돌면서 정수 좌표를 찾는다.
+        List<Pair> pairList = findPairList(line);
+        
+        // 좌표를 String[]으로 변환하기
+        return changeMap(pairList);
+    }
+    
+    public String[] changeMap(List<Pair> pairs) {
+        // map의 사이즈 구하기
+        long maxR = Long.MIN_VALUE;
+        long maxC = Long.MIN_VALUE;
+        long minR = Long.MAX_VALUE;
+        long minC = Long.MAX_VALUE;
+        
+        for (Pair p : pairs) {
+            maxR = Math.max(maxR, p.y);
+            maxC = Math.max(maxC, p.x);
+            minR = Math.min(minR, p.y);
+            minC = Math.min(minC, p.x);
         }
-//
-//        for (Point p : pointList) {
-//            System.out.print("x = " + p.x);
-//            System.out.println(" y = " + p.y);
-//        }
-
-        // 2. 사각형의 가로, 세로 길이를 구한 뒤 사각형 이차원 배열을 선언한다.
-        String[][] angle = makeAngle(pointList);
-
-        for (String[] arr : angle) {
-            Arrays.fill(arr, ".");
+        
+        int r = (int) (maxR - minR);
+        int c = (int) (maxC - minC);
+        
+        String[][] map = new String[r+1][c+1];
+        for (int i = 0; i <= r; i++) Arrays.fill(map[i], ".");
+        
+        for (Pair p : pairs) {
+            int nr = (int) (maxR - p.y);
+            int nc = (int) (p.x - minC);
+            
+            map[nr][nc] = "*";
         }
-
-        // 3. 사분면 상의 교점들을 이차원 배열에 옮긴다.
-        for (Point p : pointList) {
-            int x = (int) (p.x - minX);
-            int y = (int) (maxY - p.y);
-
-            angle[y][x] = "*";
+        
+        String[] answer = new String[map.length];
+        
+        for (int i=0; i<map.length; i++) {
+            answer[i] = "";
+            for (int j=0; j<map[0].length; j++)
+                answer[i] += map[i][j];
         }
-
-        String[] answer = new String[angle.length];
-
-        for (int i = 0; i < angle.length; i++) {
-            answer[i] = String.join("", angle[i]);
-        }
-
+        
         return answer;
     }
-
-    private String[][] makeAngle(List<Point> pointList) {
-
-        for (Point p : pointList) {
-            maxX = Math.max(maxX, p.x);
-            minX = Math.min(minX, p.x);
-            maxY = Math.max(maxY, p.y);
-            minY = Math.min(minY, p.y);
+    
+    public List<Pair> findPairList(int[][] line) {
+        List<Pair> pairList = new ArrayList<>();
+        
+        for (int i=0; i<line.length; i++) {
+            long A = line[i][0];
+            long B = line[i][1];
+            long E = line[i][2];
+            
+            for (int j=i+1; j<line.length; j++) {
+                double C = line[j][0];
+                double D = line[j][1];
+                double F = line[j][2];
+                
+                // 평행 또는 일치하는 경우
+                if (A*D - B*C == 0) continue;
+                
+                // 교점 구하기
+                double x = (B*F - E*D) / (A*D - B*C);
+                double y = (E*C - A*F) / (A*D - B*C);
+                
+                // 교점이 정수인지 판단
+                if (x % 1 != 0 || y % 1 != 0) continue;
+                
+                // 교점 리스트 추가
+                pairList.add(new Pair((long) x, (long)y));
+            }
         }
-
-        int width = (int) (maxX - minX + 1);
-        int height = (int) (maxY - minY + 1);
-
-        return new String[height][width];
-    }
-
-    private Point getPoint(double A, double B, double E,
-                           double C, double D, double F) {
-        // 1-1. 평행하거나 일치하는 경우 제외하기
-        if (A * D - B * C == 0) return null;
-
-        // 1-2. 교점 구하기
-        double x = (B * F - E * D) / (A * D - B * C);
-        double y = (E * C - A * F) / (A * D - B * C);
-
-        // 1-3. 교점이 정수가 아닌 경우 제외하기
-        if (x % 1 != 0 || y % 1 != 0) return null;
-
-        return new Point((long) x, (long) y);
-    }
-
-    public static void main(String[] args) {
-        int[][] array = {
-                {2, -1, 4},
-                {-2, -1, 4},
-                {0, -1, 1},
-                {5, -8, -12},
-                {5, 8, 12}
-        };
-
-        Solution solution = new Solution();
-        solution.solution(array);
+        
+        return pairList;
     }
 }
