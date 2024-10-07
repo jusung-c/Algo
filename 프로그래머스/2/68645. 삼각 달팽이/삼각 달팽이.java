9a66e95f95b7652e700dafd3b79df3b2c63ea1ce
이주성 <32183520@dankook.ac.kr>
2024-10-07 14:58:30 +0900

    [level 2] Title: 삼각 달팽이, Time: 53.69 ms, Memory: 170 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,61 +1,61 @@
-import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.*;
 
 class Solution {
-    // 아래로 -> 오른쪽으로 -> 왼쪽 대각선 위로
-    int[] dx = {0, 1, -1};
-    int[] dy = {1, 0, -1};
+    // 1. 방향 순서 정의 하 -> 우 -> 왼쪽위 대각선
+    static int[][] dir = new int[][] {
+            {1, 0}, {0, 1}, {-1, -1}
+    };
 
     public int[] solution(int n) {
-        int[] answer = {};
 
-        // n x n 배열 생성
+        // nxn map을 선언하고 사용하지 않는 부분은 -1 처리
         int[][] map = new int[n][n];
-
-        // map 채우기
-        map = visit(map, n);
-
-        return Arrays.stream(map)      // Stream<int[]>
-                .flatMapToInt(Arrays::stream)   // Stream<IntStream>
-                .filter(i -> i != 0)
-                .toArray();
-    }
-
-    private int[][] visit(int[][] map, int n) {
-        int v = 1;
-        int d = 0;
-        int x = 0;
-        int y = 0;
-
-        while (true) {
-            // 방문
-            map[y][x] = v++;
-
-            // 진행했던 방향 그대로 진행할 수 있는지 판단
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (nx == n || nx == -1 || ny == n || ny == -1 || map[ny][nx] != 0) {
-
-                // 진행할 수 없는 경우 다음 방향으로 진행할 수 있는지 체크
-                d = (d + 1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
-
-                // 다음 방향으로도 진행 못하면 탈출
-                if (nx == n || nx == -1 || ny == n || ny == -1 || map[ny][nx] != 0) break;
-
-            }
-
-            // 정해진 다음 좌표로 설정
-            x = nx;
-            y = ny;
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) map[i][j] = -1;
         }
 
-        return map;
-    }
+        // (0, 0)에서 시작해서 숫자 채우기
+        int index = 1;
+        int r = 0;
+        int c = 0;
+        int d = 0;
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        solution.solution(4);
+        while (true) {
+            map[r][c] = index++;
+
+            // 다음 위치 후보
+            int nr = r + dir[d][0];
+            int nc = c + dir[d][1];
+
+            // 범위를 벗어났거나 빈칸이 아닌 경우 방향 변경
+            if (nr<0||nc<0||nr>=n||nc>=n|| map[nr][nc]!=0) {
+                // 다음 방향 선정
+                d = (d+1) % 3;
+                nr = r + dir[d][0];
+                nc = c + dir[d][1];
+
+                // 다음 방향으로도 이동 못하는 경우 제외
+                if (nr<0||nc<0||nr>=n||nc>=n|| map[nr][nc]!=0)
+                    break;
+
+                // 이동
+                r = nr;
+                c = nc;
+            } else {
+                // 이동 가능한 경우 이동
+                r = nr;
+                c = nc;
+            }
+        }
+
+        // 하나의 배열로 반환하기
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<=i; j++)
+                answer.add(map[i][j]);
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
