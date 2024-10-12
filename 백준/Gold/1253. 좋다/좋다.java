249db347a918c2644694d762284187494a523d69
이주성 <32183520@dankook.ac.kr>
2024-10-12 19:19:45 +0900

    [Gold IV] Title: 좋다, Time: 172 ms, Memory: 16016 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,56 +1,59 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static BufferedReader br;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
     static int N;
     static int[] arr;
 
     public static void init() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
+        // N(1 ≤ N ≤ 2,000)개의 수열. 각 원소는 -10억 ~ 10억
+        // "좋다": 어떤 수가 다른 수 2개의 합으로 나타낼 수 있는 경우
+        // "좋은" 수의 개수를 구하라
+
         N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
         arr = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
     }
 
     private static void pro() throws IOException {
+        // 배열 정렬
         Arrays.sort(arr, 1, N + 1);
 
-        int cnt = 0;
-
-        for (int target = 1; target <= N; target++) {
-            int R = N;
-            int sum = 0;
-
-            for (int L = 1; L <= N; L++) {
-                if (L == target) continue;
-                if (R == target) R--;
-
-                sum = arr[L] + arr[R];
-
-                while (R - 1 > L && sum > arr[target]) {
-                    R--;
-                    if (R == target) continue;
-                    sum = arr[L] + arr[R];
-                }
-
-                if (sum == arr[target]) {
-//                    bw.write(arr[L] + " " + arr[R] + " target: " + arr[target]);
-//                    bw.newLine();
-                    cnt++;
-                    break;
-                }
-            }
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if (tp(i)) answer++;
         }
 
-        bw.write(cnt + " ");
+        bw.write(answer + " ");
+    }
 
+    private static boolean tp(int target) {
+        // 계산할 때 자료형은 long을 써야한다.
+        int L = 1;
+        int R = N;
+        long sum = 0;
+
+        while (L < R) {
+            // target인 경우는 제외
+            if (L == target) L++;
+            else if (R == target) R--;
+
+            if (L >= R) break;
+
+            sum = arr[L] + arr[R];
+            if (sum == arr[target]) return true;
+            else if (sum > arr[target]) R--;
+            else L++;
+        }
+
+        return false;
     }
 
     public static void main(String[] args) throws Exception {
