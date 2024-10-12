8d30056fb4b924fb85472b5e62bf0d098d36b105
이주성 <32183520@dankook.ac.kr>
2024-10-13 00:54:54 +0900

    [Gold V] Title: 수 고르기, Time: 340 ms, Memory: 28600 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,51 +1,42 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static BufferedReader br;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
     static int N, M;
     static int[] arr;
 
     public static void init() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+        arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
     }
 
     private static void pro() throws IOException {
-        Arrays.sort(arr, 1, N + 1);
-
-        int R = 0;
-        int diff = 0;
+        int dist = 0;
         int min = Integer.MAX_VALUE;
 
-        for (int L = 1; L <= N; L++) {
-            if (R != 0) {
-                diff = arr[R] - arr[L];
-            }
+        // 1 3 5
 
-            while (R + 1 <= N && diff < M) {
+        for (int R = 0, L = 0; L < N; L++) {
+            dist = arr[R] - arr[L];
+
+            while (R + 1 < N && dist < M) {
                 R++;
-                diff = arr[R] - arr[L];
+                dist = arr[R] - arr[L];
             }
 
-            if (diff >= M) {
-                min = Math.min(min, diff);
-            }
+            if (dist >= M) min = Math.min(min, dist);
         }
 
         bw.write(min + " ");
-
     }
 
     public static void main(String[] args) throws Exception {
