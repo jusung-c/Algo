0dceb06c36807091df4faab00674292010ed8275
이주성 <32183520@dankook.ac.kr>
2024-10-13 01:49:31 +0900

    [Gold III] Title: 세 용액, Time: 212 ms, Memory: 16540 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.java
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -1,57 +1,52 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static BufferedReader br;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
     static StringTokenizer st;
-    static int N, A, B, C;
+    static int N;
     static int[] arr;
 
     public static void init() throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
     }
 
     private static void pro() throws IOException {
         Arrays.sort(arr);
-
         long ans = Long.MAX_VALUE;
         long A = 0;
         long B = 0;
         long C = 0;
 
         for (int i = 0; i < N - 2; i++) {
-            long target = arr[i];
             int L = i + 1;
             int R = N - 1;
+            long sum = 0;
 
             while (L < R) {
-                long sum = target + arr[L] + arr[R];
+                sum = (long) arr[L] + arr[R] + arr[i];
 
                 if (Math.abs(sum) < ans) {
                     ans = Math.abs(sum);
-                    A = target;
+                    A = arr[i];
                     B = arr[L];
                     C = arr[R];
                 }
 
-                if (sum > 0) {
-                    R--;
-                } else {
-                    L++;
-                }
+                if (sum > 0) R--;
+                else L++;
             }
         }
 
-        bw.write(A + " " + B + " " + C + " ");
+        bw.write(A + " ");
+        bw.write(B + " ");
+        bw.write(C + " ");
     }
 
     public static void main(String[] args) throws Exception {
