d6141cfcc4f5de6bb81a485b763add08782fbe9c
이주성 <32183520@dankook.ac.kr>
2024-10-16 22:02:47 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 27.83 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,45 +1,45 @@
 class Solution {
-    // 1. 상태 정의
-    // (offsetX, offsetY)로 시작하고, 한 변의 길이가 size인 정사각형의 0과 1의 개수
-    private class Count {
+    public class State {
         int zero, one;
-
-        public Count(int zero, int one) {
+        
+        State(int zero, int one) {
             this.zero = zero;
             this.one = one;
         }
-
-        public Count add(Count o) {
-            return new Count(zero + o.zero, one + o.one);
+        
+        State add(State os) {
+            return new State(this.zero + os.zero, this.one + os.one);
         }
     }
-
-    private Count getCount(int offsetX, int offsetY, int size, int[][] arr) {
-
-        // 2. 종료 조건 탈출
-        // 정사각형 내의 원소가 모두 1이거나 모두 0일 때 종료
-        for (int x = offsetX; x < offsetX + size; x++) {
-            for (int y = offsetY; y < offsetY + size; y++) {
-
-                if (arr[y][x] != arr[offsetY][offsetX]) {
-                    int h = size / 2;
-
-                    // 3. 점화식 정의
-                    // 4개의 정사각형으로 쪼갠 뒤 재귀 호출로 압축된 결과를 더해주기
-                    return getCount(offsetX, offsetY, h, arr)
-                            .add(getCount(offsetX + h, offsetY, h, arr))
-                            .add(getCount(offsetX, offsetY + h, h, arr))
-                            .add(getCount(offsetX + h, offsetY + h, h, arr));
+    
+    public State press(int startX, int startY, int length, int[][] map) {
+        // 1. 현재 상태가 압축할 수 있는 상태인지 확인
+        for (int r=startX; r<startX + length; r++) {
+            for (int c=startY; c<startY + length; c++) {
+                if (map[r][c] != map[startX][startY]) {
+                    int nLength = length / 2;
+                    
+                    // 2. 압축이 불가능한 경우 쪼개서 다시 압축 시도
+                    return press(startX, startY, nLength, map)
+                        .add(press(startX + nLength, startY, nLength, map))
+                        .add(press(startX, startY + nLength, nLength, map))
+                        .add(press(startX + nLength, startY + nLength, nLength, map));
                 }
             }
         }
-
-        // 종료 조건 탈출
-        return arr[offsetY][offsetX] == 1 ? new Count(0, 1) : new Count(1, 0);
+        
+        // 3. 압축이 가능하다면 압축
+        return map[startX][startY] == 1 ? new State(0, 1) : new State(1, 0);
     }
-
-    public int[] solution(int[][] arr) {
-        Count count = getCount(0, 0, arr.length, arr);
-        return new int[]{count.zero, count.one};
+    
+    public int[] solution(int[][] arr) {        
+        // 이차원 배열 쿼드 압축
+        // 내부의 모든 수가 같은 값이라면 그 하나의 수로 압축
+        // 내부의 수가 다르다면 4개의 정사각형으로 쪼개 압축 시도
+        
+        // 재귀적으로 압축 시도
+        State answer = press(0, 0, arr.length, arr);
+            
+        return new int[] {answer.zero, answer.one};
     }
 }
