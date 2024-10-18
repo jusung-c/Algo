0d23c35a48fcdbda7540bea15bb272e5d776d661
이주성 <32183520@dankook.ac.kr>
2024-10-18 23:42:57 +0900

    [level 1] Title: K번째수, Time: 0.54 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,24 +1,24 @@
-import java.util.stream.IntStream;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        // 배열의 i~j번째 숫자까지 자르고 정렬했을 때 k번째에 있는 수를 구하라
+        int[] newArray;
         int[] answer = new int[commands.length];
-
-        for (int i = 0; i < commands.length; i++) {
-
-            int start = commands[i][0];
-            int end = commands[i][1];
-            int index = commands[i][2];
-
-            int[] sorted = IntStream.range(start - 1, end) // IntStream
-                    .map(n -> array[n])     // IntStream
-                    .sorted()               // IntStream
-                    .toArray();// int[]
-
-
-            answer[i] = sorted[index - 1];
-        }
-
+        
+        for (int t=0; t<commands.length; t++) {
+            int i = commands[t][0];
+            int j = commands[t][1];
+            newArray = new int[j-i+1];
+            for (int index=0, k=i-1; k<j; k++) {
+                newArray[index++] = array[k];
+            }
+            
+            Arrays.sort(newArray);
+            answer[t] = newArray[commands[t][2] - 1];
+        }   
+        
         return answer;
+        
     }
 }
