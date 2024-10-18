fa9d407031d1b5b986e134338a91293cba504bfd
이주성 <32183520@dankook.ac.kr>
2024-10-18 19:29:11 +0900

    [level 2] Title: 카펫, Time: 8.39 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,22 +1,27 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-
-        // 가로 길이 x, 세로 길이 y일 때
-        // brown: 2x + 2y - 4
-        // yellow: (x-2) * (y-2)
-
-        // 갈색과 노랑의 최소가 8, 1 이라는 것은 x와 y가 최소 3이라는 것. 즉, 3 <= y <= x
-        // 갈색의 최대가 5_000 이므로 가로의 최대가 2500을 넘지 않는다.
-        for (int x = 0; x < 2500; x++) {
-            for (int y = 0; y <= x; y++) {
-                if (brown == (2 * x + 2 * y - 4) && yellow == (x - 2) * (y - 2)) {
-                    return new int[]{x, y};
+        // 테두리는 갈색, 나머지는 노란색으로 칠해진 카펫
+        // 갈색, 노란색 격자의 개수가 주어질 때 카펫의 가로, 세로 길이를 구하라 (가로 >= 세로)
+        
+        // 가로 x, 세로 y일 때 brown: 2x + 2y - 4이고, yellow: (x-2)(y-2)이다. 
+        // brown: 2(x + y) - 4
+        // yellow: x*y -2(x + y) + 4
+        // brown + yellow = x * y
+        
+        // yellow가 1이상 -> 가로 세로가 최소 3 이상
+        // brown은 8이상 5000이하 -> 가로 세로는 5000이하
+        
+        for (int x=3; x<=5000; x++) {
+            for (int y=3; y<=x; y++) {
+                int brownCnt = 2*x + 2*y - 4;
+                int yellowCnt = (x-2)*(y-2);
+                
+                if (brownCnt == brown && yellowCnt == yellow) {
+                    return new int[] {x, y};
                 }
-
             }
         }
-
-        return answer;
+        
+        return null;
     }
 }
