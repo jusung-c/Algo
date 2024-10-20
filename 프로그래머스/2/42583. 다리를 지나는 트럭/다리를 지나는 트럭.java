63af0cf54a31ab60d5647c8d2cd39fb6eb90664e
이주성 <32183520@dankook.ac.kr>
2024-10-20 20:45:00 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 41.14 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,57 +1,63 @@
 import java.util.*;
 
 class Solution {
-    static class Truck {
-        int weight, rDist;
-        Truck(int weight, int rDist) {
+    class Truck {
+        int weight; // 트럭의 무게
+        int move;   // 트럭의 이동한 거리
+
+        Truck(int weight, int move) {
             this.weight = weight;
-            this.rDist = rDist;
+            this.move = move;
         }
-        
-        void oneSecAfter() {
-            this.rDist--;
+
+        void moving() {
+            this.move++;
         }
     }
-    
-    public int solution(int bridge_length, int weight, int[] truck) {
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Deque<Truck> waitQ = new ArrayDeque<>();    // 트럭 대기열
+        Deque<Truck> moveQ = new ArrayDeque<>();    // 다리
         
-        Deque<Truck> bridge = new ArrayDeque<>();
-        int time = 0;
-        int curWeight = 0;
+        // 대기열에 트럭 저장
+        for (int i : truck_weights) waitQ.add(new Truck(i, 0));
         
-        for (int truckWeight : truck) {
+        int curW = 0;   // 현재 다리의 무게
+        int time = 0;   // 흘러간 시간
+        
+        // 대기열과 다리 위의 트럭이 존재하지 않을 때까지 반복
+        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
             
-            // 다음 트럭 올라탈 수 있는 무게가 될 때까지 시간 보내기
-            while (curWeight + truckWeight > weight) {
-                for (Truck t : bridge) {
-                    t.oneSecAfter();
-                    
-                    // 다리의 끝에 도달한 경우
-                    if (t.rDist == 0) {
-                        curWeight -= t.weight;
-                        bridge.removeFirst();
-                    }
-                }
-                time++;
-            }
-                        
-            // 트럭을 새로 진입시키고 1초 경과 시키기
-            bridge.addLast(new Truck(truckWeight, bridge_length));
-            for (Truck t : bridge) {
-                t.oneSecAfter();
-                    
-                // 다리의 끝에 도달한 경우
-                if (t.rDist == 0) {
-                    curWeight -= t.weight;
-                    bridge.removeFirst();
-                }
-            }
-            
-            curWeight += truckWeight;
+            // 매 반복마다 1초 경과
             time++;
+            
+            // 다리에 트럭이 없는 경우
+            if (moveQ.isEmpty()) {
+                // 대기열의 트럭이 다리에 진입한다.
+                Truck t = waitQ.poll();
+                moveQ.add(t);
+                curW += t.weight;
+                continue;
+            }
+            
+            // 다리 위의 트럭들 1초간 이동
+            for (Truck t : moveQ) t.moving();
+            
+            // 도착한 트럭이 있는 경우 제외
+            if (moveQ.peek().move == bridge_length) {
+                Truck t = moveQ.poll();
+                curW -= t.weight;
+            }
+            
+            // 다 이동시킨 뒤에 새로운 트럭 올릴 수 있으면 올리기
+            if (!waitQ.isEmpty() && curW + waitQ.peek().weight <= weight) {
+                // 대기열의 트럭이 다리에 진입한다.
+                Truck t = waitQ.poll();
+                moveQ.add(t);
+                curW += t.weight;
+            }
         }
         
-        time += bridge.removeLast().rDist + 1;
         return time;
     }
 }
